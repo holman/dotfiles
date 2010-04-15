a6ce37a02d4d0dd18ce474dacf02fee2ca1d96ed
Zach Holman <zach@zachholman.com>
2010-04-15 02:36:03 -0700

    IMPROVING EVERYTHING BY 100

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,18 +1,15 @@
-alias gems='cd $GEM_HOME/gems'
+alias gems='cd /Library/Ruby/Gems/1.8/gems'
 
-# jruby
+alias jgems='cd /usr/local/Cellar/jruby/1.4.0/lib/ruby/gems/1.8/gems'
 alias f='RAILS_ENV=fi'
 
-# rails
 alias sc='script/console'
-alias ss='script/server -p `~/.dotfiles/zsh/functions/available_rails_port`'
+alias ss='script/server -p `available_rails_port`'
 alias sg='script/generate'
 alias sd='script/destroy'
 alias a='autotest -rails'
 alias tlog='tail -f log/development.log'
 alias scaffold='script/generate nifty_scaffold'
 alias migrate='rake db:migrate db:test:clone'
-alias rst='touch tmp/restart.txt'
 alias rcov='rake rcov:all'
 alias s="ps aux | grep \"[r]uby\" | grep script/server || echo \"You're not running any, dawg.\""
-alias deathss='~/.dotfiles/zsh/functions/deathss'
