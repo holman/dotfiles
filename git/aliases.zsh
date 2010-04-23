ad0452a260e13e06945384d5ddbf1a6761e40810
Zach Holman <github.com@zachholman.com>
2010-04-22 17:13:11 -0700

    sometimes I keep state secrets in unpushed branches, what.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,7 +2,7 @@ function git(){hub "$@"}
 alias gl='git pull'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias glr='git pull --rebase'
-alias gp='git push'
+alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
