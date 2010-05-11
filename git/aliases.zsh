f1076265d5a1ba7dc6a722245d1f22c5b27e8f08
Zach Holman <github.com@zachholman.com>
2010-05-11 13:23:27 -0700

    rebase and prune pulls

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,7 +1,6 @@
 function git(){hub "$@"}
-alias gl='git pull'
+alias gl='git pull --rebase --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias glr='git pull --rebase'
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
