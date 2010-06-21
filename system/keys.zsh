9acc6cf6c973c666f39485712bf5dcce8e82555d
Zach Holman <zach@zachholman.com>
2010-06-21 14:43:28 -0700

    change key location

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,2 +1,2 @@
 alias hosts="vim ~/.ssh/known_hosts"
-alias pubkey="more ~/.ssh/holman.public | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey="more ~/.ssh/id_dsa.public | pbcopy | echo '=> Public key copied to pasteboard.'"
