c54514718ea4d61c9eb7967e0ab0bfa674e61560
Zach Holman <zach@zachholman.com>
2010-07-26 14:53:59 -0700

    dominatrixing my ssh hosts

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,2 +1,6 @@
-alias hosts="vim ~/.ssh/known_hosts"
+# Remove the hosts that I don't want to keep around- in this case, only
+# keep the first couple hosts. Like a boss.
+alias hosts="head -2 ~/.ssh/known_hosts | tail -2 > ~/.ssh/known_hosts"
+
+# Pipe my public key to my clipboard. Fuck you, pay me.
 alias pubkey="more ~/.ssh/id_dsa.public | pbcopy | echo '=> Public key copied to pasteboard.'"
