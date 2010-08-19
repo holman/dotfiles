da0b7d3d2fba7c008e713797800f04fabe5ec1c1
Sumeet Jain <sumeet@sumeetjain.com>
2010-08-20 02:15:00 +0800

    This moves the space after the current ruby for RVM into rvm_prompt, so people without RVM won't have the extra space prepended to their prompt. Yes, it's very important.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -52,13 +52,13 @@ need_push () {
 rvm_prompt(){
   if $(which rvm &> /dev/null)
   then
-	  echo "%{\e[1;33m%}$(~/.rvm/bin/rvm-prompt i)%{\e[0m%} ·"
+	  echo "%{\e[1;33m%}$(~/.rvm/bin/rvm-prompt i)%{\e[0m%} · "
 	else
 	  echo ""
   fi
 }
 
-export PROMPT=$'$(rvm_prompt) %{\e[0;36m%}%1/%{\e[0m%} › '
+export PROMPT=$'$(rvm_prompt)%{\e[0;36m%}%1/%{\e[0m%} › '
 set_prompt () {
   export RPROMPT="$(need_push)$(project_name_color)$(git_dirty)"
 }
