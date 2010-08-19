e8e0d4d1caad5e77c431782ec4d717dd71c77000
Zach Holman <github.com@zachholman.com>
2010-08-18 17:39:33 -0700

    only alias if gls exists

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,7 +1,10 @@
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-alias ls="gls -F --color"
-alias l="gls -lAh --color"
-alias ll="gls -l --color"
-alias la='gls -A --color'
+if $(gls &>/dev/null)
+then
+  alias ls="gls -F --color"
+  alias l="gls -lAh --color"
+  alias ll="gls -l --color"
+  alias la='gls -A --color'
+fi
