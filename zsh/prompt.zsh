5c833136bc74622791e658632a1a621d6ebb7ca1
Zach Holman <zach@zachholman.com>
2011-03-23 21:24:48 -0700

    only show todos that are +next

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -59,10 +59,13 @@ rvm_prompt(){
   fi
 }
 
+# This keeps the number of todos always available the right hand side of my
+# command line. I filter it to only count those tagged as "+next", so it's more
+# of a motivation to clear out the list.
 todo(){
   if $(which todo.sh &> /dev/null)
   then
-    num=$(echo $(todo.sh ls | wc -l))
+    num=$(echo $(todo.sh ls +next | wc -l))
     let todos=num-2
     if [ $todos != 0 ]
     then
