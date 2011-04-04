43b284baa439d83a8ff3dd3dfdae463a3ee7315f
Zach Holman <github.com@zachholman.com>
2011-04-04 11:37:09 -0700

    chill with the rebase

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -13,7 +13,7 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --rebase --prune'
+alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
