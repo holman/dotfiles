838ce1ad517da20be61da11208cec00484f31f40
rossputin <ross@bheap.co.uk>
2011-04-11 10:25:58 +0100

    Add accurate window titling to shell for all locations

---
+++ zsh/window.zsh
@@ -0,0 +1,19 @@
+# Direct from http://dotfiles.org/~_why/.zshrc
+# Sets the window title nicely no matter where you are
+function title() {
+  # escape '%' chars in $1, make nonprintables visible
+  a=${(V)1//\%/\%\%}
+
+  # Truncate command, and join lines.
+  a=$(print -Pn "%40>...>$a" | tr -d "\n")
+
+  case $TERM in
+  screen)
+    print -Pn "\ek$a:$3\e\\"      # screen title (in ^A")
+    ;;
+  xterm*|rxvt)
+    print -Pn "\e]2;$2 | $a:$3\a" # plain xterm title
+    ;;
+  esac
+}
+
