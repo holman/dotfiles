77caff49c6db179305d07aacb6a46901d9d7230b
Zach Holman <zach@zachholman.com>
2011-05-10 18:37:29 -0700

    no more weird rails start aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -3,10 +3,7 @@ alias r='rvm use 1.8.7'
 alias f='RAILS_ENV=fi'
 
 alias sc='script/console'
-alias ss='script/server -p `available_rails_port`'
 alias sg='script/generate'
 alias sd='script/destroy'
 
 alias migrate='rake db:migrate db:test:clone'
-
-alias s="ps aux | grep \"[r]uby\" | grep script/server || echo \"You're not running any, dawg.\""
