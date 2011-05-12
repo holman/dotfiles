92ace844d20e6470114d62cae8910e126894b60e
Zach Holman <zach@zachholman.com>
2011-05-12 14:12:29 -0700

    Revert "fuck yeah completions (thanks, @Resure)"
    (zomg I kind of hated this)
    
    This reverts commit 14721e4576fd3f84999c85195aa6c0e5e509bf50.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,6 +3,3 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
-
-# completions with menu
-zstyle ':completion:*' menu yes select
