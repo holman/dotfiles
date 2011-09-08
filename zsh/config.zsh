e2158b14132dfb0ac0ef65f632888907f3843c55
Zach Holman <zach@zachholman.com>
2011-09-08 16:01:06 -0700

    set complete_aliases

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -33,6 +33,10 @@ setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share 
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
+# don't expand aliases _before_ completion has finished
+#   like: git comm-[tab]
+setopt complete_aliases
+
 zle -N newtab
 
 bindkey '^[^[[D' backward-word
