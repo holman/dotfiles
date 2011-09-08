c98ee0f2c096a6179c3861bcbb536e4b547385e5
Zach Holman <zach@zachholman.com>
2011-09-08 12:13:56 -0700

    tweak some `defaults` settings on osx

---
+++ osx/set-defaults.sh
@@ -0,0 +1,14 @@
+# Sets reasonable OS X defaults.
+#
+# Or, in other words, set shit how I like in OS X.
+#
+# The original idea (and a couple settings) were grabbed from:
+#   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+#
+# Run ./set-defaults.sh and you'll be good to go.
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
