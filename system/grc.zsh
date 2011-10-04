6e78cd3756c21adca34cacaefd2c74d74949e642
Mike Kenyon <mkenyon@zynga.com>
2011-10-04 15:31:18 -0700

    Update grc.zsh so that it doesn't complain about a lack of grc or brew

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,21 @@
+#!/bin/zsh
 # GRC colorizes nifty unix tools all over the place
-if $(grc &>/dev/null)
-then
-  source `brew --prefix`/etc/grc.bashrc
-fi
+unset touse
+for prog in grc identify; do
+  (( $+commands[$prog] )) || continue
+  touse=$prog
+  break
+done
+# (( $+touse )) || { echo "Couldn't find grc" ; return 1 }
+# echo "Found grc"
+
+unset touse
+for prog in brew identify; do
+  (( $+commands[$prog] )) || continue
+  touse=$prog
+  break
+done
+# (( $+touse )) || { echo "Couldn't find brew" ; return 1 }
+# echo "Found brew"
+
+source `brew --prefix`/etc/grc.bashrc
