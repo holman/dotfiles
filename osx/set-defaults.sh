85484222c3c585ac0e9c5ea267a263c239646d5d
Zach Holman <github.com@zachholman.com>
2011-10-09 21:46:30 -0400

    airdrop über alles

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -10,5 +10,8 @@
 # Disable press-and-hold for keys in favor of key repeat
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+# Use AirDrop over every interface. srsly this should be a default.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
 # Show the ~/Library folder
 chflags nohidden ~/Library
