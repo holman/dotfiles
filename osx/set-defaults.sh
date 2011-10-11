0675a982300a197a38758d3d7bbd68e5516a337c
Zach Holman <github.com@zachholman.com>
2011-10-11 10:24:32 -0700

    finder opens in list views

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -13,5 +13,8 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 # Use AirDrop over every interface. srsly this should be a default.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
+# Always open everything in Finder's list view. This is important.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
 # Show the ~/Library folder
 chflags nohidden ~/Library
