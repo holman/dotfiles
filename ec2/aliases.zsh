f0ef24d1cd5bb0cae109732b197a9dc262cf470d
Colin <davis.colin.w@gmail.com>
2012-03-11 23:04:03 -0700

    Added vim surround

--- ec2/aliases.zsh
+++ ec2/aliases.zsh
@@ -1,2 +1,2 @@
 alias instances="ec2-describe-instances"
-alias mars="ssh -i ~/.ec2/Mars.pem ubuntu@ec2-50-16-121-242.compute-1.amazonaws.com"
+alias mars="ssh -i ~/.ec2/Mars.pem ubuntu@50.112.110.232"
