2718a60d7d3fdf31425c1ff6162db79a4c2885fa
Mike Kenyon <mkenyon@zynga.com>
2012-03-14 09:04:03 -0700

    Change from spaces to tabs in nextbirthdays.py
    I received an IndentationError while running the script. Some lines
    had been set as tabs while the rest were spaces. I figured that I
    would set the entire file to tabs so that editors can easily set
    their editor to their preferred tab width.

--- bin/nextbirthdays.py
+++ bin/nextbirthdays.py
@@ -11,58 +11,58 @@ newThreadHL2 = 'http://bit.ly/pOwSba'
 newThreadSuggestionThreshold = 1
 
 class Birthday:
-  def __init__(self, name, month, date):
-    self.name = name
-    self.birthday = datetime.date(today.year, int(month), int(date))
-    while self.birthday < today:
-      self.birthday = self.birthday.replace(year=today.year + 1)
+	def __init__(self, name, month, date):
+		self.name = name
+		self.birthday = datetime.date(today.year, int(month), int(date))
+		while self.birthday < today:
+			self.birthday = self.birthday.replace(year=today.year + 1)
 
 def load(filepath):
-  f = open(filepath, 'r')
-  results = []
-  for line in f.readlines():
-    d = line.split(';')
-    b = Birthday(d[0], month = d[1], date = d[2])
-    results.append(b)
-  return results
+	f = open(filepath, 'r')
+	results = []
+	for line in f.readlines():
+		d = line.split(';')
+		b = Birthday(d[0], month = d[1], date = d[2])
+		results.append(b)
+	return results
 
 def isPlural(number):
-  return number != 1
+	return number != 1
 
 def getNext(birthdays):
-  results = []
-  for birthday in birthdays:
-    if birthday.birthday - today < kReportingWindowsTimeDelta:
-      results.append(birthday)
-  return results
+	results = []
+	for birthday in birthdays:
+		if birthday.birthday - today < kReportingWindowsTimeDelta:
+			results.append(birthday)
+	return results
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description='Find nearby birthdays.')
-  parser.add_argument('birthdays', help='path to the list of birthdays')
-  args = parser.parse_args()
-  birthdays = load(args.birthdays)
-  upcoming = getNext(birthdays)
-  suggestNewThread = False
-  print "Birthdays:"
-  for b in upcoming:
-    daysDiff = (b.birthday - today).days
-    if daysDiff <= newThreadSuggestionThreshold:
-      suggestNewThread = True
-	if isPlural(daysDiff):
-		daysPluralized = 'days'
-	else:
-		daysPluralized = 'day'
-    if daysDiff > 0:
-      daysCountdownText = 'in ' + str(daysDiff) + ' ' + daysPluralized
-    else:
-      daysCountdownText = 'today!'
+	parser = argparse.ArgumentParser(description='Find nearby birthdays.')
+	parser.add_argument('birthdays', help='path to the list of birthdays')
+	args = parser.parse_args()
+	birthdays = load(args.birthdays)
+	upcoming = getNext(birthdays)
+	suggestNewThread = False
+	print "Birthdays:"
+	for b in upcoming:
+		daysDiff = (b.birthday - today).days
+		if daysDiff <= newThreadSuggestionThreshold:
+			suggestNewThread = True
+		if isPlural(daysDiff):
+			daysPluralized = 'days'
+		else:
+				daysPluralized = 'day'
+		if daysDiff > 0:
+			daysCountdownText = 'in ' + str(daysDiff) + ' ' + daysPluralized
+		else:
+			daysCountdownText = 'today!'
 
-    print b.name, 'has a birthday', daysCountdownText 
-  if suggestNewThread:
-    print 'TF2?', newThreadTF2
-    print 'HL2?', newThreadHL2
-  if len(upcoming) == 0:
-	print 'No birthdays in the next %i' % kReportingWindowsDays,
+		print b.name, 'has a birthday', daysCountdownText 
+	if suggestNewThread:
+		print 'TF2?', newThreadTF2
+		print 'HL2?', newThreadHL2
+	if len(upcoming) == 0:
+		print 'No birthdays in the next %i' % kReportingWindowsDays,
 	if isPlural(kReportingWindowsDays):
 		print 'days'
 	else:
