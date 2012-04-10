10859427f04e5101f359cde8998a300a0b35a135
Zach Holman <zach@zachholman.com>
2012-04-09 20:13:38 -0700

    fix rbenv

---
+++ ruby/rbenv.zsh
@@ -0,0 +1,17 @@
+# rehash shims
+rbenv rehash 2>/dev/null
+
+# shell thing
+rbenv() {
+  command="$1"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  shell)
+    eval `rbenv "sh-$command" "$@"`;;
+  *)
+    command rbenv "$command" "$@";;
+  esac
+}
