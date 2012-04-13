ba22165f3846a70a146f8365cd05d27963b883a8
Zach Holman <zach@zachholman.com>
2012-04-12 17:16:04 -0700

    Load up hub again

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,19 +1,9 @@
 # Use `hub` as our git wrapper:
 #   http://defunkt.github.com/hub/
-#
-# I use JRuby substantially, and we want to make sure hub is run using MRI
-# regardless of which Ruby you're using or else the `git status` in your prompt
-# will take seven thousand seconds to run `ls`.
-#
-# I'm hardcoding it to an installed rvm (using rvm's `rvm 1.8.7,ruby /hub/path`
-# syntax is way too slow). It should work fine for those without rvm, though.
-if [[ -s $HOME/.rvm/scripts/rvm ]]
+hub_path=$(which hub)
+if [[ -f $hub_path ]]
 then
-  if $(which hub &> /dev/null) && [[ -s $HOME/.rvm/rubies/ruby-1.8.7-p334 ]]
-  then
-    alias git='$HOME/.rvm/rubies/ruby-1.8.7-p358/bin/ruby `which hub`'
-  else
-  fi
+  alias git=$hub_path
 fi
 
 # The rest of my fun git aliases
