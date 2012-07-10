20fe21e3ddc418c57e90158607d778a42a2e8c3f
Mike Kenyon <mkenyon@zynga.com>
2012-07-09 18:13:41 -0700

    add alias gdc=git diff --cached

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -22,6 +22,7 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
+alias gdc='git diff --cached'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
