f2057545c0fd6fc02de47da17c058229d37828c8
Julio Santos <julio@morgane.com>
2012-10-13 22:35:24 -0700

    clean up errors

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -72,7 +72,7 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
 }
