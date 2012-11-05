2b228449af2d5a4754d0c074438224ec90107f34
Julio Santos <julio@morgane.com>
2012-11-04 20:45:01 -0800

    tweaks

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,17 +6,18 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
+alias gb='git branch'
+alias gbav='gb -avv'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias gd='git diff'
+alias gl='git pull --prune'
 alias glod='git log --oneline --decorate --graph --abbrev-commit --date=relative'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='git push origin HEAD'
+# alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias grm='git status -s | awk '\''/^ D/{for (i=2; i<=NF; i++) { printf("%s ", $i)} printf("\n")}'\'' | xargs git rm'
-alias gbav='gb -avv'
+alias gs='git status -sb'
+alias gsbn='git log --name-only | grep -v "^ " | grep \.rb$ | sort | uniq -c | sort -nr'
+alias gitrank='git shortlog -sn'
