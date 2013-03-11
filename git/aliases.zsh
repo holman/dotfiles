0252af7d6df25f5bfc89cb2c5e1b495b8442ca90
Julio Santos <julio@morgane.com>
2013-03-11 07:22:02 -0700

    tweaks

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,10 +1,10 @@
 # Use `hub` as our git wrapper:
 #   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if [[ -f $hub_path ]]
-then
-  alias git=$hub_path
-fi
+# hub_path=$(which hub)
+# if [[ -f $hub_path ]]
+# then
+#   alias git=$hub_path
+# fi
 
 alias gb='git branch'
 alias gbav='gb -avv'
