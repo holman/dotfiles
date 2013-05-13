4f3666ba6a450c9d9f67402cf4105b67c6f244d7
Travers McInerney <tmcinerney@zendesk.com>
2013-05-13 14:07:45 +1000

    Tidied things up

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,6 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/local/share/npm/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export NODE_PATH="/usr/local/lib/node:$NODE_PATH"
+export ZENDESK_ROOT="~/code/zendesk"
+#eval "$(/Users/travers/Development/Zendesk/z/bin/z init -)"
+source /opt/boxen/env.sh
--- zsh/path.zsh
+++
@@ -1,1 +0,0 @@
-source /opt/boxen/env.sh
