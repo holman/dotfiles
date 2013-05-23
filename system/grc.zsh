671e534f993ecf2ca5e0b69f26b880a741dccf71
Travers McInerney <travers@mcinerney.me>
2013-05-23 22:44:49 +1000

    Use holman style for loading configuration

--- system/grc.zsh
+++ system/grc.zsh
@@ -3,3 +3,4 @@ if $(grc &>/dev/null) && ! $(brew &>/dev/null)
 then
   source `brew --prefix`/etc/grc.bashrc
 fi
+
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -4,34 +4,31 @@
 #  export PS1='%3~$(git_info_for_prompt)%# '
 #fi
 
-##export LSCOLORS="exfxcxdxbxegedabagacad"
-##export CLICOLOR=true
+fpath=($DOT/functions $fpath)
 
-##fpath=($DOT/functions $fpath)
+autoload -U $DOT/functions/*(:t)
 
-##autoload -U $DOT/functions/*(:t)
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
 
-##HISTFILE=~/.zsh_history
-##HISTSIZE=10000
-##SAVEHIST=10000
-
-##setopt NO_BG_NICE # don't nice background tasks
+setopt NO_BG_NICE # don't nice background tasks
 ##setopt NO_HUP
-##setopt NO_LIST_BEEP
-##setopt LOCAL_OPTIONS # allow functions to have local options
-##setopt LOCAL_TRAPS # allow functions to have local traps
-##setopt HIST_VERIFY
-##setopt SHARE_HISTORY # share history between sessions ???
-##setopt EXTENDED_HISTORY # add timestamps to history
+setopt NO_LIST_BEEP
+setopt LOCAL_OPTIONS # allow functions to have local options
+setopt LOCAL_TRAPS # allow functions to have local traps
+setopt HIST_VERIFY
+setopt SHARE_HISTORY # share history between sessions ???
+setopt EXTENDED_HISTORY # add timestamps to history
 ##setopt PROMPT_SUBST
 ##setopt CORRECT
 ##setopt COMPLETE_IN_WORD
 ##setopt IGNORE_EOF
 
-##setopt APPEND_HISTORY # adds history
-##setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-##setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-##setopt HIST_REDUCE_BLANKS
+setopt APPEND_HISTORY # adds history
+setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_REDUCE_BLANKS
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
