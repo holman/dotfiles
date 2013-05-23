7aa45ce12f807d0f3b02c9f6772fa8554d3381b3
Travers McInerney <travers@mcinerney.me>
2013-05-23 23:06:00 +1000

    Updated system path variables

--- system/path.zsh
+++ system/path.zsh
@@ -1,7 +1,2 @@
 [ -f /opt/boxen/env.sh ] && source /opt/boxen/env.sh
 
-#export BOXEN_PATH="/opt/boxen"
-#export HOMEBREW_PATH="$BOXEN_PATH/homebrew"
-#export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/local/share/npm/bin:$HOMEBREW_PATH/bin:$HOMEBREW_PATH/sbin:$PATH"
-#export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
