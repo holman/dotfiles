c6f99e7617fc95de6151cb8be30d7434b77f1900
Bradley Wagner <bradley.wagner@gmail.com>
2013-05-30 14:41:42 -0400

    Added more of my .zsh aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,7 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+alias md5-deep='find -s . -type f -exec md5 {} \; >> file.md5'
+alias dnscacheclear='sudo killall -HUP mDNSResponder'
+alias uninstall-gems='gem list | cut -d" " -f1 | xargs sudo gem uninstall -aIx'
