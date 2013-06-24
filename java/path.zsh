4e4748847f198546acf4d70b16f3b2b5026c82dd
Bradley Wagner <bradley.wagner@gmail.com>
2013-06-24 14:46:35 -0400

    Exporting JAVA_HOME too for Ant and Tomcat

--- java/path.zsh
+++ java/path.zsh
@@ -3,8 +3,9 @@ ANT_HOME=$HOME/Dropbox/Dev/Java/ant
 MAVEN_OPTS='-Xmx768m -noverify'
 M2_HOME=/usr/share/maven
 M2=$M2_HOME/bin
-#JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_15.jdk/Contents/Home
-JAVA_HOME=/Library/Java/JavaVirtualMachines/1.6.0_45-b06-451.jdk/Contents/Home
+JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_15.jdk/Contents/Home
+#JAVA_HOME=/Library/Java/JavaVirtualMachines/1.6.0_45-b06-451.jdk/Contents/Home
 JAVA_OPTS=-Xmx512m
 
+export JAVA_HOME
 export PATH="$JAVA_HOME/bin:$ANT_HOME/bin:$PATH"
