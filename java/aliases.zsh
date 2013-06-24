748c9903f74f32356a425ab06b80da735bfdbb73
Bradley Wagner <bradley.wagner@gmail.com>
2013-06-24 19:01:18 -0400

    Adding aliases to switch java versions

---
+++ java/aliases.zsh
@@ -0,0 +1,2 @@
+alias j6='export JAVA_HOME=$J6_HOME; export PATH=$JAVA_HOME/bin:$PATH'
+alias j7='export JAVA_HOME=$J7_HOME; export PATH=$JAVA_HOME/bin:$PATH'
--- java/path.zsh
+++ java/path.zsh
@@ -3,9 +3,12 @@ ANT_HOME=$HOME/Dropbox/Dev/Java/ant
 MAVEN_OPTS='-Xmx768m -noverify'
 M2_HOME=/usr/share/maven
 M2=$M2_HOME/bin
-JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_15.jdk/Contents/Home
-#JAVA_HOME=/Library/Java/JavaVirtualMachines/1.6.0_45-b06-451.jdk/Contents/Home
+J6_HOME=/Library/Java/JavaVirtualMachines/1.6.0_45-b06-451.jdk/Contents/Home
+J7_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_15.jdk/Contents/Home
+JAVA_HOME=$J6_HOME
 JAVA_OPTS=-Xmx512m
 
+export J6_HOME
+export J7_HOME
 export JAVA_HOME
 export PATH="$JAVA_HOME/bin:$ANT_HOME/bin:$PATH"
