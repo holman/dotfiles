ae7570b5cc357dbdfbff7bb6e8dc43da96f37f23
Gibson Starkweather <gibson@gibson.fm>
2013-07-03 14:41:18 -0700

    Fix seds treatment of tabs
    Per https://github.com/holman/dotfiles/commit/3d2a856d2475f8c7e6cff6117652f92542cb6247#commitcomment-3561553.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,4 +17,4 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
-           sed 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
+           perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
