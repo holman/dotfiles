ac32048182ec13ce63ffa244232058b723c5f9ef
Travers McInerney <travers@mcinerney.me>
2013-07-09 16:50:02 +1000

    Updated path for z

--- zendesk/path.zsh
+++ zendesk/path.zsh
@@ -1,3 +1,3 @@
 export ZENDESK_ROOT=~/Code/zendesk
 export ZENDESK_PROVISIONING_PATH=~/Code/zendesk/zendesk_provisioning
-eval "$(/Users/tmcinerney/code/zendesk/z/bin/z init -)"
+eval "$(~/Code/zendesk/z/bin/z init -)"
