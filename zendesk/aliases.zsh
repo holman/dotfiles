cadb882e25823e3df3d6e7d0a41492362a107faf
Travers McInerney <travers@mcinerney.me>
2013-07-16 10:26:38 +1000

    Added alias for apps

--- zendesk/aliases.zsh
+++ zendesk/aliases.zsh
@@ -1,4 +1,5 @@
 alias zd="cd ~/Code/zendesk"
+alias apps="cd ~/Code/zendesk/apps"
 alias classic="cd ~/Code/zendesk/zendesk"
 alias carson="cd ~/Code/zendesk/zendesk_carson"
 alias console="cd ~/Code/zendesk/zendesk_console"
