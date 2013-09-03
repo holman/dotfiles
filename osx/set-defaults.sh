d7bbe4c39f560a868052af57513511d58ccfa288
redhotvengeance <rhv@redhotvengeance.com>
2013-09-02 22:41:17 -0700

    Add restart-services to osx

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -5,7 +5,7 @@
 # The original idea (and a couple settings) were grabbed from:
 #   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
 #
-# Run ./set-defaults.sh and you'll be good to go.
+# Run ./bin/set-defaults and you'll be good to go.
 
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
