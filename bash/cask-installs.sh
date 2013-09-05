77ba22afbee411eedc340acf046b3bfe614d464b
Steven Bone <stevenmbone@gmail.com>
2013-09-05 11:28:40 -0400

    Added script for Cask installs

---
+++ bash/cask-installs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+brew cask install adium
+brew cask install alfred
+brew cask install charles
+brew cask install dropbox
+brew cask install evernote
+brew cask install f-lux
+brew cask install firefox
+brew cask install imageoptim
+brew cask install iterm2
+brew cask install one-password
+brew cask install optimal-layout
+brew cask install the-unarchiver
+brew cask install transmit
+
+echo "All Casks installed!"
