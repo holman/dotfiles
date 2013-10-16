0c896ba620343b73391dd1a0baa23e984bd25353
Zach Holman <zach@zachholman.com>
2013-10-15 18:28:48 -0700

    install ruby-build, too

--- ruby/install.sh
+++ ruby/install.sh
@@ -5,3 +5,9 @@ then
   echo "  Installing rbenv for you."
   brew install rbenv > /tmp/rbenv-install.log
 fi
+
+if test ! $(which ruby-build)
+then
+  echo "  Installing ruby-build for you."
+  brew install ruby-build > /tmp/ruby-build-install.log
+fi
