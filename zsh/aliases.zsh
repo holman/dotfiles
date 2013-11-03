a9d59c4316d6f141cf309f81fa3e4cd1a2e41ed3
cwardzala <cwardzala@gmail.com>
2013-11-03 11:53:22 -0500

    first round of personal changes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,6 @@
 alias reload!='. ~/.zshrc'
+alias zshconfig="subl ~/.zshrc"
+alias ohmyzsh="subl ~/.oh-my-zsh"
+alias edit="subl ."
+alias fww="~/Findawayworld"
+alias proj="~/Projects"
---
+++ zsh/path.zsh
@@ -0,0 +1,10 @@
+# Customize to your needs...
+export PATH=/usr/local/bin:$PATH
+export PATH=$PATH:/usr/local/share/npm/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:$HOME/.rvm/bin
+export PATH=/Applications/Postgres.app/Contents/MacOS/bin:$PATH
+export PAHT=/usr/local/Cellar/flex_sdk/4.6.0.23201/libexec/bin:$PATH
+export FLEX_HOME=/usr/local/Cellar/flex_sdk/4.6.0.23201/libexec
+export SASS_LIBSASS_PATH=/Users/cwardzala/Projects/libsass
+export HDHOMERUN_PATH=/Users/cwardzala/tools/libhdhomerun/libhdhomerun.dylib
+### Added by the Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
