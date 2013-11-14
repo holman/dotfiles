64d71c12d3a0b310e91d528344357f677164f9f7
Ole Christian Langfjæran <judoole@gmail.com>
2013-11-14 12:32:31 +0100

    Marks for quickly navigating

---
+++ zsh/marks.zsh
@@ -0,0 +1,28 @@
+# Quick navigation from the command line
+# http://jeroenjanssens.com/2013/08/16/quickly-navigate-your-filesystem-from-the-command-line.html
+# Usage :
+# $ cd ~/some/very/deep/often-used/directory
+# $ mark deep
+# $ jump deep
+# $ unmark deep
+# $ marks
+export MARKPATH=$HOME/.marks
+function jump { 
+    cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
+}
+function mark { 
+    mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1"
+}
+function unmark { 
+    rm -i "$MARKPATH/$1"
+}
+function marks {
+    \ls -l "$MARKPATH" | tail -n +2 | sed 's/  / /g' | cut -d' ' -f9- | awk -F ' -> ' '{printf "%-10s -> %s\n", $1, $2}'
+}
+
+function _completemarks {
+  reply=($(ls $MARKPATH))
+}
+
+compctl -K _completemarks jump
+compctl -K _completemarks unmark
