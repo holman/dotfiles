2735d988ee0dcf3bc810c76111e8635a6750e58c
Steven Bone <sbone@vhx.tv>
2013-12-02 20:34:20 -0500

    Modify a couple Cask installs

--- bash/cask-installs.sh
+++ bash/cask-installs.sh
@@ -2,6 +2,7 @@
 
 brew cask install adium
 brew cask install alfred
+brew cask install appcleaner
 brew cask install charles
 brew cask install dropbox
 brew cask install evernote
@@ -10,9 +11,10 @@ brew cask install firefox
 brew cask install google-chrome
 brew cask install imageoptim
 brew cask install iterm2
-brew cask install keyremap4macbook
+brew cask install mailplane
 brew cask install one-password
 brew cask install optimal-layout
+brew cask install owncloud
 brew cask install the-unarchiver
 brew cask install transmit
 brew cask install lastfm
