9e596ca8e579e462a76ae6edd8c13099a8a17612
Ole Christian Langfjæran <judoole@gmail.com>
2014-01-13 10:01:39 +0100

    Some Java proxy stuff

--- sg/proxy.zsh
+++ sg/proxy.zsh
@@ -6,6 +6,8 @@ function proxy() {
    export HTTP_PROXY=http://no-sfd6-websec1.z42.no.tconet.net:80
    export https_proxy=http://no-sfd6-websec1.z42.no.tconet.net:80
    export HTTPS_PROXY=http://no-sfd6-websec1.z42.no.tconet.net:80
+   export GRADLE_OPTS_ORIG=$GRADLE_OPTS
+   export GRADLE_OPTS="$GRADLE_OPTS_ORIG -Dhttp.proxyHost=no-sfd6-websec1.z42.no.tconet.net"
    export ALL_PROXY=$http_proxy
    echo $ALL_PROXY
  else
@@ -14,6 +16,7 @@ function proxy() {
    export HTTP_PROXY=
    export https_proxy=
    export HTTPS_PROXY=
+   export GRADLE_OPTS=$GRADLE_OPTS_ORIG
    export ALL_PROXY=
  fi
 }
