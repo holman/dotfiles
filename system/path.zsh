37c5e15d05761ba9a3cf8564f549303a58a1ea60
tmilewski <tmilewski@gmail.com>
2014-01-21 15:52:51 -0500

    add Postgres.app to PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,8 @@
+# rbenv
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
+# Postgres.app
+export PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"
+
+# MySQL
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
