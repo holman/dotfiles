f5885c494bac165ca0bec2d1917eb9507b75c578
Ole Christian Langfjæran <judoole@gmail.com>
2014-01-29 20:53:23 +0100

    Gradle updates. Better wrapper search

--- gradle/install.sh
+++ gradle/install.sh
@@ -4,4 +4,6 @@ if test ! $(which gradle)
 then
   echo "  Installing gradle for you."
   brew install gradle > /tmp/gradle-install.log
+  #wget http://services.gradle.org/distributions/gradle-1.10-bin.zip -O /tmp/gradle.zip; rm -rf /opt/gradle/; mkdir -p /opt/gradle/current/; unzip /tmp/gradle.zip -d /opt/current;rm /tmp/gradle.zip
+  ln -s $PWD/find-gradle /usr/local/bin/gw
 fi
