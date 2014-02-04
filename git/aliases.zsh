10b2626668b475a116c30555066e7e6e73278c83
Zach Holman <zach@zachholman.com>
2014-02-04 15:40:16 -0800

    Copy branch name to clipboard with `gcb`

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -14,6 +14,7 @@ alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
+alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
