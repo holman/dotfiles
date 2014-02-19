c2fddaee5e7e2b8f487f1d71f250652e985b15b0
Ole Christian Langfjæran <judoole@gmail.com>
2014-02-19 20:48:45 +0100

    Some cd aliases from @mathiasbynens

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,9 @@
 alias reload!='. ~/.zshrc'
+
+alias ..="cd .."
+alias dl="cd ~/Downloads"
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en1"
+alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
