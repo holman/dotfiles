c7a8d39919b867c201407e8957ba60b7e30e2af8
Ole Christian Langfjæran <judoole@gmail.com>
2014-02-20 10:57:32 +0100

    Node does checks for not exists

--- node/install.sh
+++ node/install.sh
@@ -1,5 +1,19 @@
-brew install npm
-# Node debugger
-npm install -g node-inspector
-# Nodemon. Restarts server when changes.
-npm install -g nodemon
+#!/bin/sh
+
+if test ! $(which npm)
+then
+  echo "  Installing npm for you."
+  brew install npm > /tmp/npm-install.log
+fi
+
+if test ! $(which node-inspector)
+then
+  echo "  Installing node-inspector for you."
+  npm install -g node-inspector > /tmp/node-inspector-install.log
+fi
+
+if test ! $(which nodemon)
+then
+  echo "  Installing nodemon for you."
+  npm install -g nodemon > /tmp/nodemon-install.log
+fi
