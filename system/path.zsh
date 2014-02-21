5fa0e0352e859773771c1b576ffaac72b596a643
Zach Holman <zach@zachholman.com>
2014-02-21 15:12:13 +1100

    sfs never went anywhere

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,2 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
