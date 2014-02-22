bd934532541631df902d7590f2a80df95a0d3701
Ole Christian Langfjæran <judoole@gmail.com>
2014-02-22 19:01:24 +0100

    mergepdf. From @mathiasbynens

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,3 +7,7 @@ alias dl="cd ~/Downloads"
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Merge PDF files
+# Usage: `mergepdf -o output.pdf input{1,2,3}.pdf`
+alias mergepdf='/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py'
