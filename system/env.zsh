12320b2e791dfbe58b531d4faa2e5721955be2d5
Zach Holman <zach@zachholman.com>
2014-02-27 09:07:56 -0700

    Quit pretending like I haven't been using Atom for over a year

--- system/env.zsh
+++ system/env.zsh
@@ -1,5 +1,1 @@
-# Only set this if we haven't set $EDITOR up somewhere else previously.
-if [[ "$EDITOR" == "" ]] ; then
-  # Use sublime for my editor.
-  export EDITOR='subl'
-fi
+export EDITOR='atom'
