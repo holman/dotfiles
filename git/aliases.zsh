60a5f55deba050e22c080269f2a44d3a2d852e3d
Zach Holman <zach@zachholman.com>
2014-03-21 18:43:08 -0700

    remove things you don't use

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,5 +17,3 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
-           perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
