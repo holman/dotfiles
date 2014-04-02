3bac32ee7234739359585127d9298f7ec8fc0a2b
cwardzala <cwardzala@gmail.com>
2014-04-02 12:51:29 -0400

    remove Homebrew GH API key from github.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -4,13 +4,12 @@ export PATH="/usr/local/bin:/usr/local/share/npm/bin:/usr/bin:/bin:/usr/sbin:/sb
 ### Added by the Heroku Toolbelt
 export PATH="/usr/local/heroku/bin:$PATH"
 export PATH="$HOME/.composer/vendor/bin:$PATH"
-#export PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"
 
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Findawayworld
 
 export FLEX_HOME=$HOME/tools/apache-flex-sdk
 
-export HOMEBREW_GITHUB_API_TOKEN=c0cd65ef1ea526e028b2ad9a9688ddc541cf6b5b
-
 export PATH="$(brew --prefix josegonzalez/php/php55)/bin:$PATH"
+
+## HOMEBREW_GITHUB_API_TOKEN moved to ~/.localrc
