f72c8c682aedb10fe21f80e7159fa38162956164
kaihowl <kai@hoewelmeyer.eu>
2014-04-05 17:34:32 +0000

    Made scm_breeze install more robust
    * stop early on errors.
    * don't copy paste internal update magic, source it!

--- scm_breeze/install.sh
+++ scm_breeze/install.sh
@@ -1,15 +1,11 @@
-#!/bin/sh
-checkout_path=~/.scm_breeze/
+#!/bin/bash
+set -e
+
+checkout_path=~/.scm_breeze
 
 if [ -d "$checkout_path" ]; then
-  currDir=$PWD 
-  cd "$scmbDir"
-  oldHEAD=$(git rev-parse HEAD 2> /dev/null) 
-  git pull origin master
-  source "$scmbDir/lib/scm_breeze.sh"
-  _create_or_patch_scmbrc $oldHEAD
-  source "$scmbDir/scm_breeze.sh"
-  cd "$currDir"
+  source "${checkout_path}/lib/scm_breeze.sh"
+  update_scm_breeze
 else
   git clone git://github.com/ndbroadbent/scm_breeze.git $checkout_path
 fi
