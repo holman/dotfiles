0008609e5f3faf7011ede7fc5540ddfefd9e0c96
kaihowl <kai@hoewelmeyer.eu>
2014-04-05 15:42:09 +0000

    Fixed installation of autojump under Ubuntu
    * elif statement was not working as intended and did not match.
    * apt-get was missing a -y flag for automatic install

--- autojump/install.sh
+++ autojump/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$(uname)" == "Darwin" ]; then
   brew install autojump
-elif [ "$(lsb_release -i)" == "Distributor ID: Ubuntu" ]; then
-  sudo apt-get install autojump
+elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
+  sudo apt-get install -y autojump
 fi
