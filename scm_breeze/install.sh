ff6d222a363aee6667253994da494b96202ff037
kaihowl <kai@hoewelmeyer.eu>
2014-04-05 15:46:29 +0200

    Calling sourced function from script will not work

--- scm_breeze/install.sh
+++ scm_breeze/install.sh
@@ -2,7 +2,14 @@
 checkout_path=~/.scm_breeze/
 
 if [ -d "$checkout_path" ]; then
-  update_scm_breeze
+  currDir=$PWD 
+  cd "$scmbDir"
+  oldHEAD=$(git rev-parse HEAD 2> /dev/null) 
+  git pull origin master
+  source "$scmbDir/lib/scm_breeze.sh"
+  _create_or_patch_scmbrc $oldHEAD
+  source "$scmbDir/scm_breeze.sh"
+  cd "$currDir"
 else
   git clone git://github.com/ndbroadbent/scm_breeze.git $checkout_path
 fi
