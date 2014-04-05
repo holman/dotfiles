7f48d79dfbb7570b190fd1698d4706f6687bd16c
kaihowl <kai@hoewelmeyer.eu>
2014-04-05 15:32:26 +0000

    Resolve macro to actual script invocation

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,7 +2,7 @@
 checkout_path=~/.oh-my-zsh
 
 if [ -d "$checkout_path" ]; then
-  upgrade_oh_my_zsh
+  /usr/bin/env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh
 else
   git clone git://github.com/robbyrussell/oh-my-zsh.git $checkout_path
 fi
