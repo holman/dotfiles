1ccbe2edcb8cab03dcfeececadcc4a97a4e61d56
kaihowl <kai@hoewelmeyer.eu>
2014-04-05 14:06:05 +0200

    Removed faulty autojump zsh file

--- autojump/autojump.zsh
+++
@@ -1,7 +0,0 @@
-# autojump
-if [ "$(uname)" = "Darwin" ]; then
-  [[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
-elif [ "$(lsb_release -i)" = "Distributor ID: Ubuntu" ]; then
-  zsh_file="$(dpkg -L autojump | grep "autojump.zsh")"
-  source $zshfile
-fi
