46a46da7c5dd502a660f533079238d4429b3dc67
kaihowl <kai@hoewelmeyer.eu>
2014-04-05 16:24:10 +0200

    Adding powerline to tmux

---
+++ powerline/set_conf_path.zsh
@@ -0,0 +1,4 @@
+# This sets the path where to find the conf/zsh/etc files for sourcing in the
+# respective configuration files
+
+export POWERLINE_DIR=`pip show powerline | grep Location | sed s/Location:\ //g`
