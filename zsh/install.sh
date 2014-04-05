63a90a9fe4a3e73d204fb444ea1ca4eedc47b468
kaihowl <kai@hoewelmeyer.eu>
2014-04-05 16:18:44 +0200

    Added missing dollar sign

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,5 +4,5 @@ checkout_path=~/.oh-my-zsh
 if [ -d "$checkout_path" ]; then
   upgrade_oh_my_zsh
 else
-  git clone git://github.com/robbyrussell/oh-my-zsh.git checkout_path
+  git clone git://github.com/robbyrussell/oh-my-zsh.git $checkout_path
 fi
