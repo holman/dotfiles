6ea0af6d54d9fac76579c3903313e467dd89c61f
kaihowl <kai@hoewelmeyer.eu>
2014-04-05 15:59:37 +0000

    Do not try to install homebrew on ubuntu

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,6 +5,11 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
+# If not OSX, just exit cleanly
+if [ "$(uname -s)" != "Darwin" ]; then
+  exit 0
+fi
+
 # Check for Homebrew
 if test ! $(which brew)
 then
