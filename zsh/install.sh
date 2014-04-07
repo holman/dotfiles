bb61a18a99bad06fbf832497f4255b53949248f8
kaihowl <kai@hoewelmeyer.eu>
2014-04-07 10:08:24 +0200

    oh-my-zsh install set ZSH var if not set

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,6 +4,10 @@ set -e
 checkout_path=~/.oh-my-zsh
 
 if [ -d "$checkout_path" ]; then
+  if [[ "x${ZSH}" == "x" ]]; then
+    # Older zsh templates did not export ZSH var
+    ZSH=$checkout_path
+  fi
   source ${checkout_path}/lib/functions.zsh
   upgrade_oh_my_zsh
 else
