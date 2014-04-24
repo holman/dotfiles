521e28843ee6b58ff7e74a1003f98cddc71862ea
Bradley Wagner <bradley.wagner@gmail.com>
2014-04-24 11:53:11 -0400

    More memory for Ant and new location for JDK 1.7

--- java/path.zsh
+++ java/path.zsh
@@ -1,10 +1,10 @@
-ANT_OPTS=-Xmx768m
+ANT_OPTS=-Xmx2048m
 ANT_HOME=$HOME/Dropbox/Dev/Java/ant
 MAVEN_OPTS='-Xmx768m -noverify'
 M2_HOME=/usr/share/maven
 M2=$M2_HOME/bin
 J6_HOME=/Library/Java/JavaVirtualMachines/1.6.0_45-b06-451.jdk/Contents/Home
-J7_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_15.jdk/Contents/Home
+J7_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home
 JAVA_HOME=$J7_HOME
 JAVA_OPTS=-Xmx512m
 
