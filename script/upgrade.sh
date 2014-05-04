b02582e3cf0ca2134a1caca21a4a2955fd0535e5
kaihowl <kai@hoewelmeyer.eu>
2014-05-04 20:17:39 +0200

    Only bootstrap if updates pulled

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -2,12 +2,18 @@
 
 printf '\033[0;34m%s\033[0m\n' "Upgrading Dotfiles"
 cd "$DOTS"
+oldcommit=$(git rev-parse master)
 if git pull --rebase --stat origin master
 then
   printf '\033[0;34m%s\033[0m\n' 'Dotfiles updated to current version.'
   printf '\033[0;34m%s\033[0m\n' 'Bootstrapping dotfiles.'
-  "$DOTS/script/bootstrap"
-  printf '\033[0;34m%s\033[0m\n' 'You might have to run script/install.'
+  newcommit=$(git rev-parse master)
+  if [[ $oldcommit != $newcommit ]]; then
+    "$DOTS/script/bootstrap"
+    printf '\033[0;34m%s\033[0m\n' 'You might have to run script/install.'
+  else
+    printf '\033[0;34m%s\033[0m\n' 'No updates found.'
+  fi
 else
   printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
 fi
