baedf84242649b628eb346b12d8366b04762bf6f
kaihowl <kai@hoewelmeyer.eu>
2014-05-07 07:02:47 +0200

    Toggling vim with ctrl-z restores prompt+input

--- zsh/togglevi.zsh
+++ zsh/togglevi.zsh
@@ -1,12 +1,7 @@
 # http://chneukirchen.org/blog/archive/2012/02/10-new-zsh-tricks-you-may-not-know.html
-# http://serverfault.com/questions/225817/toggle-foreground-process
 foreground-vi() {
-  if [[ $#BUFFER -eq 0 ]]; then
-    fg %?vi
-    zle redisplay
-  else
-    zle push-input
-  fi
+  fg %?vi
+  zle redisplay
 }
 zle -N foreground-vi
 bindkey '^Z' foreground-vi
