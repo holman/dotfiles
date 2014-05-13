1dd4d1fefb236dd122a58a05f35e836f528cc99c
OmeGak <omegak@gmail.com>
2014-05-14 00:22:45 +0200

    More verbose install script and pretty prints
    * Install script is more verbose
    * Install script prints list of dot.log files created
    * Pretty prints bundled for use with install.sh files

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,18 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Homebrew
 #
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
+if [[ "$(uname -s)" != "Darwin" ]]; then
+  pprint error "Unsupported OS"
+  exit 0
+fi
+
 # Check for Homebrew
-if test ! $(which brew)
-then
-  echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" > /tmp/homebrew-install.log
+if test ! $(which brew); then
+  pprint step "Installing Homebrew for you"
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" > /tmp/homebrew-install.dot.log 2>&1
+
+  if [[ $? != 0 ]]; then
+    pprint error "Failed to install Homebrew"
+    exit 1
+  else
+    pprint ok "Homebrew is ready to brew. Choo, choo!"
+  fi
 fi
 
 # Install homebrew packages
-brew install grc coreutils spark
+brew install coreutils grc > /tmp/homebrew-dependencies.dot.log 2>&1
 
+if [[ $? != 0 ]]; then
+  pprint error "Failed to install dependencies"
+  exit 1
+fi
+
+pprint ok "Installed dotfiles dependencies"
 exit 0
--- ruby/install.sh
+++ ruby/install.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
-if test ! $(which rbenv)
-then
-  echo "  Installing rbenv for you."
-  brew install rbenv > /tmp/rbenv-install.log
+if test ! $(which rbenv); then
+  pprint step "Installing rbenv for you"
+  brew install rbenv > /tmp/rbenv-install.dot.log 2>&1
+
+  if [[ $? != 0 ]]; then
+    pprint error -r "Failed to install rbenv"
+    errors=true
+  fi
 fi
 
-if test ! $(which ruby-build)
-then
-  echo "  Installing ruby-build for you."
-  brew install ruby-build > /tmp/ruby-build-install.log
+if test ! $(which ruby-build); then
+  pprint step "Installing ruby-build for you"
+  brew install ruby-build > /tmp/ruby-build-install.dot.log 2>&1
+
+  if [[ $? != 0 ]]; then
+    pprint error -r "Failed to install ruby-build"
+    errors=true
+  fi
 fi
+
+if [[ $errors == "true"]]; then
+	exit 1
+fi
+
+pprint ok "Ruby is properly installed"
+exit 0
