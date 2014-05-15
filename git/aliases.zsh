dbab08db0c864d8ba2a3ab927f6035cdb754284c
kaihowl <kai@hoewelmeyer.eu>
2014-05-15 12:31:29 +0200

    Added git submodule aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -20,3 +20,5 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gsmu='git submodule update --init'
+alias gsms='git submodule sync'
