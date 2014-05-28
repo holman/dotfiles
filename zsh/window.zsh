5756c272e455d93565eea9d94bf9444bc9b7fe44
kaihowl <kai@hoewelmeyer.eu>
2014-05-28 21:34:52 +0200

    Enable zsh custom title function in screen-256color

--- zsh/window.zsh
+++ zsh/window.zsh
@@ -8,7 +8,7 @@ function title() {
   a=$(print -Pn "%40>...>$a" | tr -d "\n")
 
   case $TERM in
-  screen)
+  screen|screen-256color)
     print -Pn "\ek$a:$3\e\\" # screen title (in ^A")
     ;;
   xterm*|rxvt)
