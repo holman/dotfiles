122946876406b456a999a3c668471d4c5f0a1967
kaihowl <kai@hoewelmeyer.eu>
2014-05-28 07:36:38 +0000

    Only show update message when actually updating

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -6,10 +6,10 @@ cd "$DOTS"
 oldcommit=$(git rev-parse master)
 if git pull --rebase --stat origin master
 then
-  printf '\033[0;34m%s\033[0m\n' 'Dotfiles updated to current version.'
-  printf '\033[0;34m%s\033[0m\n' 'Bootstrapping dotfiles.'
   newcommit=$(git rev-parse master)
   if [[ $oldcommit != $newcommit ]]; then
+    printf '\033[0;34m%s\033[0m\n' 'Dotfiles updated to current version.'
+    printf '\033[0;34m%s\033[0m\n' 'Bootstrapping dotfiles.'
     "$DOTS/script/bootstrap"
     printf '\033[0;34m%s\033[0m\n' 'You might have to run script/install.'
   else
