c5b7f13e09d99eb0f461fab835fdfcbde43d1520
kaihowl <kai@hoewelmeyer.eu>
2014-05-28 21:35:41 +0200

    Set custom cmd:pwd title in tmux windows

--- zsh/window.zsh
+++ zsh/window.zsh
@@ -7,9 +7,16 @@ function title() {
   # Truncate command, and join lines.
   a=$(print -Pn "%40>...>$a" | tr -d "\n")
 
+  workingdir=$(print -Pn "%~")
+  curcmd=$(print -Pn "$a")
+
   case $TERM in
   screen|screen-256color)
-    print -Pn "\ek$a:$3\e\\" # screen title (in ^A")
+    if [[ $workingdir == $curcmd ]]; then
+      print -Pn "\ek$a\e\\" # screen title (in ^A")
+    else
+      print -Pn "\ek$a:%~\e\\" # screen title (in ^A")
+    fi
     ;;
   xterm*|rxvt)
     print -Pn "\e]2;$2\a" # plain xterm title ($3 for pwd)
