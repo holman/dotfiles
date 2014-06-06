cb274b4db43e2fed5d89e0b64da431dea5ea7865
kaihowl <kai@hoewelmeyer.eu>
2014-06-06 06:21:25 +0200

    Init / update vundle in script/install

--- vim/install.sh
+++ vim/install.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-checkout_path=~/.vim/bundle/vundle
-
-if [ -d "$checkout_path" ]; then
-  cd $checkout_path
-  git pull --rebase
-else
-  git clone https://github.com/gmarik/vundle.git $checkout_path
-fi
+# update packages in vundle
+vundle
