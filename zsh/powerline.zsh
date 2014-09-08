d2e98f8f27db89fccccb7d226e553d3d44ccb6f6
crimsonknave <crimsonknave@gmail.com>
2014-09-08 16:23:16 -0400

    Tweaking for older boxen

--- zsh/powerline.zsh
+++ zsh/powerline.zsh
@@ -1,4 +1,7 @@
-powerline-daemon -q
+if [ $POWERLINE_CAPABLE ]
+then
+  powerline-daemon -q
 
-. ~/.local/lib/**/powerline/bindings/zsh/powerline.zsh
+  . ~/.local/lib/**/powerline/bindings/zsh/powerline.zsh
+fi
 
