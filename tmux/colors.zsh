a0c27d0f0e9059cfc3df30090307b95228e82040
crimsonknave <crimsonknave@gmail.com>
2014-09-08 12:54:18 -0400

    Tweaking powerline in awesome, and fiddling with TERM

--- tmux/colors.zsh
+++ tmux/colors.zsh
@@ -1,1 +1,8 @@
-TERM=screen-256color
+ls /usr/share/terminfo/*|grep screen-256color 2&>1 > /dev/null
+code=$?
+if [[ $code = 0 ]]
+then
+  TERM=screen-256color
+else
+  TERM=xterm-256color
+fi
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,3 @@
 alias reload!='. ~/.zshrc'
 unalias sl
+alias sshc='ssh-copy-id -i ~/.ssh/id_rsa.pub'
