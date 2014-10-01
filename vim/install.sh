e441f367935fdc9c4aa1d3b24cd22e95c6d7ad0b
kaihowl <kai@hoewelmeyer.eu>
2014-10-01 14:26:02 +0200

    Install vundle if not present

--- vim/install.sh
+++ vim/install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 # update packages in vundle
+
+# install vundle if not already installed
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 zsh -c 'source $ZSH/plugins/vundle/vundle.plugin.zsh; vundle'
