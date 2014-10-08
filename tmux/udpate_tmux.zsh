65f35599c82fa8344a5224d490038eb0391bb05e
kaihowl <kai@hoewelmeyer.eu>
2014-10-08 11:10:16 +0200

    Really silence tmux reload of conf with version 1.8

--- tmux/udpate_tmux.zsh
+++ tmux/udpate_tmux.zsh
@@ -1,3 +1,5 @@
 #!/usr/bin/zsh
 # Update running tmux sessions with possibly new tmux conf
+tmux set-option quiet on
 tmux source-file ~/.tmux.conf &> /dev/null
+tmux set-option quiet off &> /dev/null
