73d0138e3f5af73d22e66a100e06a2586ed28386
kaihowl <kai@hoewelmeyer.eu>
2014-10-08 11:20:52 +0200

    Fix quiet tmux: Initial loading was still verbose
    Instead of silencing the binary from outside, we now simply set the quiet
    option at the start of each tmux session

--- tmux/udpate_tmux.zsh
+++ tmux/udpate_tmux.zsh
@@ -1,5 +1,3 @@
 #!/usr/bin/zsh
 # Update running tmux sessions with possibly new tmux conf
-tmux set-option quiet on
 tmux source-file ~/.tmux.conf &> /dev/null
-tmux set-option quiet off &> /dev/null
