fbb60f58e44e4059ab64f9b445e0c26efdf7d566
Zach Holman <zach@zachholman.com>
2014-10-08 20:15:27 -0700

    Yosemite changed the path of iOS Simulator.app

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,1 +1,1 @@
-alias ios="open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app"
+alias ios="open /Applications/Xcode.app/Contents/Applications/iOS\ Simulator.app"
