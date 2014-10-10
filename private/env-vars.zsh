f5fbf5de3f5a6ecfc60814397014e13b24b398b9
Zach Holman <zach@zachholman.com>
2014-10-10 14:21:41 -0700

    Add private environment variable loading

---
+++ private/env-vars.zsh
@@ -0,0 +1,4 @@
+# Stash your environment variables in ~/.env-vars. This means they'll stay out
+# of your main dotfiles repository (which may be public, like this one), but
+# you'll have access to them in your scripts.
+source ~/.env-vars
