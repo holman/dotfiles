2dedab872dd12c87877d1881f55c6662a67b9345
kaihowl <kai@hoewelmeyer.eu>
2014-10-10 10:58:02 +0200

    Add zsh zmv configuration

---
+++ zsh/zmv.zsh
@@ -0,0 +1,7 @@
+# Load and configure zsh move function for easier renaming of files with
+# patterns
+autoload zmv
+alias zmz='noglob zmv'
+alias zcp='noglob zmv -C'
+alias zln='noglob zmv -L'
+alias zsy='noglob zmv -Ls'
