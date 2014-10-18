eb9b4c62c1291c84e98df9427ed79d39ff0e4743
Daan van Dijk <drvdijk@gmail.com>
2014-10-18 15:15:58 -0400

    Not outputting to /tmp files which hid that the user needs to press enter at some point.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,7 +9,7 @@
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Install homebrew packages
