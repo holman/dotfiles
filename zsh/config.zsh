433a44f1172a7925082419645f0c11c1118b430d
imlazyone <imlazyone@gmail.com>
2014-10-22 23:26:38 +0800

    Use oh-my-zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -7,9 +7,9 @@ fi
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
-fpath=($ZSH/functions $fpath)
+fpath=($ZSHDOT/functions $fpath)
 
-autoload -U $ZSH/functions/*(:t)
+autoload -U $ZSHDOT/functions/*(:t)
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
