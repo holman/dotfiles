3406692c9a9e926b20d777c6ff75831217e14637
Luiz Damim <luizdamim@outlook.com>
2014-12-05 14:28:54 -0200

    Update git aliases and base config

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,13 +7,3 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gcb='git copy-branch-name'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
