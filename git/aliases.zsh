ddff56010bd180b4ff0f8929d1ce54187abf5a63
Zach Holman <zach@zachholman.com>
2014-12-23 16:18:03 -0600

    Add git-post-push-compare-url
    This is a script that allows us to print the URL of the Compare View for
    a particular push.
    
    Git doesn't have a post-push hook because of weird pedantic hand wavery
    reasons, so instead I'm tying this into `gp`, which is my alias for
    pushing to the remote. The porcelain version of the command is used and
    piped to `git-post-push-compare-url`, which simply checks to see if it's
    a github.com URL and then prints it to stdout. From there I can
    triple-click the line to quickly copy it and paste it elsewhere if I
    want. I didn't want to go so far as to pipe it to `open` or something,
    although that could be a possibility, too.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,7 +9,7 @@ fi
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gp='git push origin HEAD --porcelain | git-post-push-compare-url'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
