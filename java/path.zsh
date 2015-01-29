d6d220598f391769e514afef3ce9d93d3fd44bb1
Bradley Wagner <bradley.wagner@gmail.com>
2015-01-29 15:28:57 -0500

    J8 and more memory

--- java/path.zsh
+++ java/path.zsh
@@ -5,8 +5,9 @@ M2_HOME=/usr/share/maven
 M2=$M2_HOME/bin
 J6_HOME=/Library/Java/JavaVirtualMachines/1.6.0_45-b06-451.jdk/Contents/Home
 J7_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_67.jdk/Contents/Home
+J8_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_20.jdk/Contents/Home
 JAVA_HOME=$J7_HOME
-JAVA_OPTS=-Xmx512m
+JAVA_OPTS=-Xmx768m
 
 export J6_HOME
 export J7_HOME
