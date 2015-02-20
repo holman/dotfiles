84e0fbdd64b8a77908078bc0103254e6aaaaf733
Luiz Damim <luizvd@gmail.com>
2015-02-20 15:22:05 -0200

    Remove hub_path aliase (not used)

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,9 +1,1 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
-
 # The rest of my fun git aliases
