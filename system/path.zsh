42c0c7f80bdb70621c52e41277a8bf8ea08351eb
Brandon Ferguson <bnferguson@github.com>
2015-03-16 12:03:55 +0100

    update paths and fix gco not autocompleting brach issue via fixing compdef

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:$PROJECTS/pages/script:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export CPPFLAGS="-I/opt/boxen/homebrew/include:$CPPFLAGS"
+export LDFLAGS="-I/opt/boxen/homebrew/lib:$LDFLAGS"
