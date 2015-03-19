28913d3dbb21a785192b730fe91b10c0b8d7ec9e
kaihowl <kai@hoewelmeyer.eu>
2015-03-19 12:07:31 +0100

    Install ag

---
+++ ag/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ "$(uname)" == "Darwin" ]; then
+  brew install the_silver_searcher
+elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
+  sudo apt-get install -y the_silver_searcher
+fi
