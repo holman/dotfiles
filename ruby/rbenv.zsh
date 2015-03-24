d6fb5fa8d2828a8a1c18fec97f7f5d924eb28fbb
Luiz Damim <luizvd@gmail.com>
2015-03-24 18:55:30 -0300

    Change RBENV_ROOT path

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,3 +1,6 @@
+# Use Homebrew's directories rather than ~/.rbenv (also per Homebrew's caveats)
+export RBENV_ROOT=/usr/local/var/rbenv
+
 # init according to man page
 if (( $+commands[rbenv] ))
 then
