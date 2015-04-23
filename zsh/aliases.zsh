b58fa21062c558e86f6c90fd91026964353a934b
Joseph Henrich <jhenrich@constantcontact.com>
2015-04-23 17:48:41 -0400

    More fucking aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,17 @@
 alias reload!='. ~/.zshrc'
+
 alias sshc='ssh-copy-id -i ~/.ssh/id_rsa.pub'
 alias ssh='TERM=xterm-256color ssh'
 alias sshig='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 alias sshcig='ssh-copy-id -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+
 alias fuck='eval $(thefuck $(fc -ln -1))'
 alias FUCK='eval $(thefuck $(fc -ln -1))'
+alias bah='eval $(thefuck $(fc -ln -1))'
+alias grr='eval $(thefuck $(fc -ln -1))'
+alias grrr='eval $(thefuck $(fc -ln -1))'
+alias grrrr='eval $(thefuck $(fc -ln -1))'
+
+alias fucking='sudo'
+alias please='sudo'
+alias yolo='sudo'
