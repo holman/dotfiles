1950561eb7cad3075f53e625db2e74356889d850
Joseph Henrich <jhenrich@constantcontact.com>
2015-04-27 16:38:49 -0400

    Added grbom alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -15,5 +15,6 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias wtf='git-wtf'
 alias gdt='git difftool'
 alias grb='git rebase'
+alias grbom='git rebase origin master'
 alias gfo='git fetch origin'
 alias gfu='git fetch upstream'
