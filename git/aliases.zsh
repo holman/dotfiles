31c3c1b5e6993d716b410364c8458a455073b227
Pranav Bhargava <pranav@jobvite.com>
2015-05-20 10:03:33 -0700

    Removing some stuff and adding some stuff.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,10 +11,14 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
+alias gundo='git reset --hard'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
+alias grbi='git rebase -i'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
+alias gsr='git svn rebase'
+alias gsd='git svn dcommit'
--- ruby/aliases.zsh
+++
@@ -1,7 +0,0 @@
-alias r='rbenv local 1.8.7-p358'
-
-alias sc='script/console'
-alias sg='script/generate'
-alias sd='script/destroy'
-
-alias migrate='rake db:migrate db:test:clone'
