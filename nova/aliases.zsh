32ba9a78ce92ff988ef3cfddfb41d1f2fe21e2c9
Joseph Henrich <jhenrich@constantcontact.com>
2015-05-29 16:07:49 -0400

    Added nova and cleaned up tmux config

---
+++ nova/aliases.zsh
@@ -0,0 +1,4 @@
+alias nova_stage='export OS_AUTH_URL=http://stage-openstackapi.ad.prodcc.net:5000/v2.0'
+alias nova_prod='export OS_AUTH_URL=http://prod-openstackapi.ad.prodcc.net:5000/v2.0'
+alias nova_qa='export OS_AUTH_URL=http://qa-openstackapi.ad.prodcc.net:5000/v2.0'
+alias nova_password='echo "Enter AD password for nova"; read -rs OS_PASSWORD ; export OS_PASSWORD'
---
+++ nova/nova_creds.zsh
@@ -0,0 +1,7 @@
+OS_USERNAME=`whoami`
+export OS_USERNAME
+OS_TENANT_NAME='admin'
+export OS_TENANT_NAME
+OS_AUTH_URL='http://qa-openstackapi.ad.prodcc.net:5000/v2.0'
+export OS_AUTH_URL
+
