a6c8d1640a306b36c4a90cf21844e77b94577366
Joseph Henrich <jhenrich@constantcontact.com>
2015-06-01 13:07:40 -0400

    added rails_env = test alias

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -3,6 +3,7 @@ alias sg='script/generate'
 alias sd='script/destroy'
 
 alias rsff="rspec --fail-fast"
+alias ret='RAILS_ENV=test '
 alias rspec="nocorrect rspec"
 alias jruby="nocorrect jruby"
 alias be='bundle exec'
