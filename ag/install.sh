9cce40b897eb5acbc859115137bd799de0e215f6
kaihowl <kai@hoewelmeyer.eu>
2015-07-01 22:08:21 +0200

    Use correct Ubuntu package name for ag/silversearcher

--- ag/install.sh
+++ ag/install.sh
@@ -2,5 +2,5 @@
 if [ "$(uname)" == "Darwin" ]; then
   brew install the_silver_searcher
 elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
-  sudo apt-get install -y the_silver_searcher
+  sudo apt-get install -y silversearcher-ag
 fi
