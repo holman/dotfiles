9b5a805d10498acbf27de7e4c040cb0ef7f13ada
Brandon Ferguson <bnferguson@github.com>
2015-07-25 17:42:10 +0200

    adjust paths

--- system/path.zsh
+++ system/path.zsh
@@ -3,3 +3,5 @@ export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:$PROJECTS/pages/script:/usr
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export CPPFLAGS="-I/opt/boxen/homebrew/include:$CPPFLAGS"
 export LDFLAGS="-I/opt/boxen/homebrew/lib:$LDFLAGS"
+export LD_LIBRARY_PATH="/usr/local/lib:/opt/boxen/homebrew/lib:$LD_LIBRARY_PATH"
+export LIBRARY_PATH="/usr/local/lib:/opt/boxen/homebrew/lib:$LIBRARY_PATH"
