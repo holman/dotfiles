8dd4d58dc3d9033cfb8dfa6fe9bc35a75d2d2514
Acho Arnold <arnold@archlinux.info>
2015-07-29 10:20:29 +0100

    Making script compatible wih /bin/sh

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -11,9 +11,11 @@ then
   echo "  Installing Homebrew for you."
 
   # Install the correct homebrew for each OS type
-  if [ "$(uname)" == "Darwin" ]; then
+  if test "$(uname)" = "Darwin"
+  then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
   fi
   
