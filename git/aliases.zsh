7674e36338053097cfa3e88949b188332f5c2d03
mattbeedle <mattbeedle@gmail.com>
2015-11-21 14:54:53 +0100

    gpf alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,6 +10,7 @@ fi
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+alias gpf='git push origin HEAD -f'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
