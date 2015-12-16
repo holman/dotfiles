180a37517e56dc723a1bae091384af248be59a82
bnferguson <bnferguson@gmail.com>
2015-12-16 13:17:42 +0100

    Add git sync, update paths

--- system/path.zsh
+++ system/path.zsh
@@ -1,7 +1,7 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:$PROJECTS/pages/script:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:$PROJECTS/pages/script:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/local/opt/go/libexec/bin:$GOPATH/bin:$PATH"
+export GOPATH=$HOME/dev
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-export CPPFLAGS="-I/opt/boxen/homebrew/include:$CPPFLAGS"
-export LDFLAGS="-I/opt/boxen/homebrew/lib:$LDFLAGS"
-export LD_LIBRARY_PATH="/usr/local/lib:/opt/boxen/homebrew/lib:$LD_LIBRARY_PATH"
-export LIBRARY_PATH="/usr/local/lib:/opt/boxen/homebrew/lib:$LIBRARY_PATH"
+# export CPPFLAGS="-I/opt/boxen/homebrew/include:$CPPFLAGS"
+# export LDFLAGS="-I/opt/boxen/homebrew/lib:$LDFLAGS"
+# export LD_LIBRARY_PATH="/usr/local/lib:/opt/boxen/homebrew/lib:$LD_LIBRARY_PATH"
+# export LIBRARY_PATH="/usr/local/lib:/opt/boxen/homebrew/lib:$LIBRARY_PATH"
