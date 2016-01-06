23168cdbf4a005cc6579be928a7583ea72781c88
Joe Critchley <joecritch@gmail.com>
2016-01-06 15:07:41 +0000

    Added shortcut for adding and committing together
    Usage: `$ gac "Initial commit"`

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,3 +17,4 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gac='git add -A && git commit -m'
