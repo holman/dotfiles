4c3d8caa6a820209098a1f035a73e26bd9dc7bf1
Javier Holguera <jholguera@JHOLGUERA.local>
2016-02-18 14:35:56 +0000

    Adding lein configuration

---
+++ lein.symlink/profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[lein-kibit "0.1.2"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
