cd6209c56d1311fb1003a4f6d852889615f2844f
Muneeb Baderoen <muneeb.baderoen@investec.co.za>
2016-03-23 11:29:58 +0200

    added hidden files show to apple defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -7,6 +7,9 @@
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
+# Show hidden files in finder
+defaults write com.apple.finder AppleShowAllFiles YES
+
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
