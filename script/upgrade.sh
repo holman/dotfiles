8fa6aed85e6ffc1faef7948db17e1a875c8034cf
kaihowl <kai@hoewelmeyer.eu>
2016-03-25 13:46:54 +0100

    Trigger oh-my-zsh update from check_for_upgrade

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -1,24 +1,6 @@
 #!/bin/zsh
-# Shamelessly copied from oh my zsh
 
-printf '\033[0;34m%s\033[0m\n' "Upgrading Dotfiles"
 cd "$DOTS"
-oldcommit=$(git rev-parse master)
-if git fetch origin && git rebase origin/master master
-then
-  newcommit=$(git rev-parse master)
-  if [[ $oldcommit != $newcommit ]]; then
-    printf '\033[0;34m%s\033[0m\n' 'Dotfiles updated to current version.'
-    printf '\033[0;34m%s\033[0m\n' 'Bootstrapping dotfiles.'
-    "$DOTS/script/bootstrap"
-    printf '\033[0;34m%s\033[0m\n' 'You might have to run script/install.'
-  else
-    printf '\033[0;34m%s\033[0m\n' 'No updates found.'
-  fi
 
-  if ! git --no-pager diff --exit-code master..origin/master > /dev/null; then
-    printf '\033[0;31m%s\033[0m\n' 'You have pending, local changes. Use dot-export.'
-  fi
-else
-  printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
-fi
+/usr/bin/env /bin/zsh ~/.oh-my-zsh/tools/upgrade.sh
+/bin/zsh $DOTS/script/upgrade_dotfiles.sh
---
+++ script/upgrade_dotfiles.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+# Shamelessly copied from oh my zsh
+
+printf '\033[0;34m%s\033[0m\n' "Upgrading Dotfiles"
+cd "$DOTS"
+oldcommit=$(git rev-parse master)
+if git fetch origin && git rebase origin/master master
+then
+  newcommit=$(git rev-parse master)
+  if [[ $oldcommit != $newcommit ]]; then
+    printf '\033[0;34m%s\033[0m\n' 'Dotfiles updated to current version.'
+    printf '\033[0;34m%s\033[0m\n' 'Bootstrapping dotfiles.'
+    "$DOTS/script/bootstrap"
+    printf '\033[0;34m%s\033[0m\n' 'You might have to run script/install.'
+  else
+    printf '\033[0;34m%s\033[0m\n' 'No updates found.'
+  fi
+
+  if ! git --no-pager diff --exit-code master..origin/master > /dev/null; then
+    printf '\033[0;31m%s\033[0m\n' 'You have pending, local changes. Use dot-export.'
+  fi
+else
+  printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
+fi
