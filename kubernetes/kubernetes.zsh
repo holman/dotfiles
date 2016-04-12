57a1ef3d8622b32776a9c880cc059b6446f28346
spiddy <dimitris.kapanidis@harbur.io>
2016-04-12 10:07:29 +0200

    added kubernetes aliases

---
+++ kubernetes/kubernetes.zsh
@@ -0,0 +1,14 @@
+# Basic kubernetes aliases
+
+# kubectl
+alias k='kubectl'
+
+# kubectl get
+alias kg='kubectl get'
+
+# kubectl get <resource>
+alias kgp='kubectl get pod'
+alias kgs='kubectl get svc'
+alias kgr='kubectl get rc'
+alias kgn='kubectl get node'
+alias kge='kubectl get events'
