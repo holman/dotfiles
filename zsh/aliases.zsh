d2e37df79f61ddb1741d81d94a4256483c8bb178
Muneeb Baderoen <muneeb.baderoen@investec.co.za>
2016-05-10 14:54:19 +0200

    recreating vimrc for neovim

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,2 @@
 alias reload!='. ~/.zshrc'
-alias chrome="open -a Google\ Chrome --args --disable-web-security"
+alias chrome="open -a Google\ Chrome --args --disable-web-security --user-data-dir=/Users/$USER/Library/Application\\ Support/Google/ChromePersonal"
