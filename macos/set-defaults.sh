36b4d59a6b416bb22ecac126b45e07d0ed45e92d
Troy Rosenberg <tmr08c@gmail.com>
2016-05-18 15:03:08 -0400

    Sets up new OSX default Screenshot directory.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -39,3 +39,7 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Change screenshot directory
+mkdir ~/Pictures/ScreenShots/
+defaults write com.apple.screencapture location ~/Pictures/ScreenShots/;killall SystemUIServer
