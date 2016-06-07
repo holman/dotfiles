fd4962d4ea83993badc5873a6a831dff6b23b601
Joseph Henrich <crimsonknave@gmail.com>
2016-06-06 23:34:48 -0400

    Updating gc to sign commits

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,6 +7,7 @@
 #fi
 
 # The rest of my fun git aliases
+alias gc='git commit -v -S'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
