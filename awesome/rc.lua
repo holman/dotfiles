4bdeb23b3fdf71a1f127f37bef502be56e37701a
Joseph Henrich <crimsonknave@gmail.com>
2016-06-06 23:38:31 -0400

    dbus-launch term

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -43,7 +43,7 @@ end
 beautiful.init("~/.dotfiles/awesome/theme/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "gnome-terminal"
+terminal = "dbus-launch gnome-terminal"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
