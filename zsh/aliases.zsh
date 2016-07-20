183a3064026999bb118680279e353cb22a01ffcc
Muneeb Baderoen <muneeb.baderoen@investec.co.za>
2016-07-20 09:38:05 +0200

    disable linewrap. more vim settings

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,3 @@
 alias reload!='. ~/.zshrc'
+alias vim='nvim'
 alias chrome="open -a Google\ Chrome --args --disable-web-security --user-data-dir=/Users/$USER/Library/Application\\ Support/Google/ChromePersonal"
