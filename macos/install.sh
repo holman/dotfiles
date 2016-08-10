07bbae423c9687c741ea30f36078ba2156a09ac1
zenom <andy@conspyre.com>
2016-08-10 17:00:10 -0400

    Another fix to not run stuff on linux

--- macos/install.sh
+++ macos/install.sh
@@ -3,5 +3,8 @@
 # command line interface to it that we can use to just install everything, so
 # yeah, let's do that.
 
-echo "› sudo softwareupdate -i -a"
-sudo softwareupdate -i -a
+
+if [[ "$(uname)" = "Darwin" ]]; then
+  echo "› sudo softwareupdate -i -a"
+  sudo softwareupdate -i -a
+fi
