5d92ed7093d39653ffa68b96068613cfd1698738
andrew.holman@phishme.com <andrew.holman@phishme.com>
2016-08-10 20:58:06 +0000

    Update Perms

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,9 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+if [[ "$(uname)" = "Linux" ]]; then
+  #alias tmux='tmux -2 -f ~/.tmux-osx.conf'
+  export PATH="$ZSH/../.linuxbrew/bin:$PATH"
+else
+  #alias tmux='tmux -2'
+fi
