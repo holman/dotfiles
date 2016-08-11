aa4608090892a81847da1739598d8d214a50eb16
zenom <andy@conspyre.com>
2016-08-10 22:01:24 -0400

    Update aliases for git

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,10 +1,6 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-
-# The rest of my fun git aliases
-alias gl='git pull --prune'
+alias gpp='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gpu='git push'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
