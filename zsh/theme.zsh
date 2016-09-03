8e4a50464d1aec1daef61a6026581a49a16c0139
Justin Luna <4a4c@users.noreply.github.com>
2016-09-03 17:21:53 -0600

    updated zsh topic now uses oh-my-zsh

---
+++ zsh/theme.zsh
@@ -0,0 +1,5 @@
+# Set name of the theme to load.
+# Look in ~/.oh-my-zsh/themes/
+# Optionally, if you set this to "random", it'll load a random theme each
+# time that oh-my-zsh is loaded.
+ZSH_THEME="robbyrussell"
