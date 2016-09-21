99fd21ef9548412fd0e94f3a98bc76226fd5fa0b
mattbeedle <me@mattbeedle.name>
2016-09-21 16:31:42 +0700

    Various fixes

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,7 +12,7 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 alias gp='git push origin HEAD'
 alias gpf='git push origin HEAD -f'
 alias gd='git diff'
-alias gc='git commit'
+alias gc='git commit -S'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
