489a036085e2c59547a78fe8b0af7998c1f9476a
Zach Holman <zach@zachholman.com>
2016-10-12 11:20:01 -0700

    remove `newtab` references (closes #256)

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -31,12 +31,9 @@ setopt HIST_REDUCE_BLANKS
 #   like: git comm-[tab]
 setopt complete_aliases
 
-zle -N newtab
-
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
 bindkey '^[[5D' beginning-of-line
 bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
