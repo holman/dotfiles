eb556e8823e723a672d25e367e4315b76f291f39
zenom <andy@conspyre.com>
2016-10-20 14:49:36 -0400

    Fix for key repeat in defaults

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -20,7 +20,7 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 chflags nohidden ~/Library
 
 # Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain KeyRepeat -float 0.000000000001
 
 # Set the Finder prefs for showing a few different volumes on the Desktop.
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
