e2a2903c7ab6df0780b7df59644573fc95776151
mattbeedle <mattbeedle@gmail.com>
2016-11-04 13:31:25 +0700

    Some linux fixes

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -16,15 +16,16 @@ then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
   then
+    sudo apt install -y build-essential gcc ruby zlib1g-dev libxslt1-dev
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
 
-    echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"' > ~/.bashrc
-    echo 'export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"' > ~/.bashrc
-    echo 'export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"' > ~/.bashrc
+    echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"' >> ~/.bashrc
+    echo 'export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"' >> ~/.bashrc
+    echo 'export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"' >> ~/.bashrc
 
-    echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"' > ~/.zshrc
-    echo 'export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"' > ~/.zshrc
-    echo 'export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"' > ~/.zshrc
+    echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"' >> ~/.zshrc
+    echo 'export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"' >> ~/.zshrc
+    echo 'export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"' >> ~/.zshrc
 
   fi
 
--- vim/install.sh
+++ vim/install.sh
@@ -12,4 +12,7 @@ mkdir -p tmp/backup tmp/swap tmp/undo
 git clone https://github.com/gmarik/vundle.git bundle/vundle
 echo "let g:vimified_packages = ['general', 'fancy', 'css', 'js', 'os', 'html', 'coding', 'color', 'ruby']" > local.vimrc
 
+ln -s ~/.extra.vimrc ~/vimified/extra.vimrc
+ln -s ~/.after.vimrc ~/vimified/after.vimrc
+
 vim +BundleInstall +qall 2>/dev/null
