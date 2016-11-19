c08546cdf600f12d927e463f1a20827c2f3f1f43
Jason Wu <jason@adstage.io>
2016-11-19 01:49:50 -0800

    More tweaks.

--- sublime3/setup.sh
+++ sublime3/setup.sh
@@ -2,10 +2,13 @@
 # Setup a machine for Sublime Text 3
 set -x
 
-# symlink settings in
+# Symlink settings
 sublime_dir=~/Library/Application\ Support/Sublime\ Text\ 3/Packages
 mv "$sublime_dir/User" "$sublime_dir/User.backup"
 ln -s "$ZSH/sublime3/User" "$sublime_dir"
 
-ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
-ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
+# Symlink sublime CLI helper
+if [[ ! -a /usr/local/bin/sublime ]]
+then
+  ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
+fi
