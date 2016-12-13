5ac0080e6bb06662a617f3bf9a86ce0e374a5067
chaadow <chaadow@msn.com>
2016-12-13 15:31:00 +0100

    projects navigation, especially listing the cdpath directories

---
+++ zsh/navigation.zsh
@@ -0,0 +1,22 @@
+_cdpath_directories() {
+  modified_in_last_days=${1:-999}
+  echo "${CDPATH//:/\n}" | while read dir; do
+    find -L "$dir" \
+      -not -path '*/\.*' \
+      -type d \
+      -atime -"$modified_in_last_days" \
+      -maxdepth 1
+  done
+}
+
+_is_a_git_repo() {
+  while read dir; do
+    if [[ -d "$dir/.git" ]]; then
+      basename "$dir"
+    fi
+  done
+}
+
+projects() {
+  _cdpath_directories $1 | _is_a_git_repo
+}
