701c81e2f66b4d0c34e91b14391aec2038598e96
Eric Ritchey <eric@eandjdesign.com>
2016-12-16 14:00:00 -0800

    atom theme updates and updates to $path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,1 @@
-export PATH="~/.themekit:~/.rbenv/bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="~/.themekit:~/.rbenv/bin:./bin:/usr/local:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
