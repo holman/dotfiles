231a823ddea6768a8f47c87639998706f61b082a
kaihowl <kai@hoewelmeyer.eu>
2016-12-28 12:27:52 +0100

    Add gcob git-checkout-force-branch alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,6 +17,7 @@ alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
+alias gcob='git checkout -B'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
