6b344f9b10be02c2d02b970fdc3f5a3d2cad86b4
chaadow <chaadow@msn.com>
2017-01-04 13:18:40 +0100

    Migrate brew packages to a Brewfile

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,9 +9,19 @@
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  fi
+
 fi
 
+exit 0
 brew tap Goles/battery
 # Install homebrew packages
 brew install grc coreutils spark git hub gibo battery fzf zsh-syntax-highlighting vim ag
