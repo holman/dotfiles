be64da3658d225cdeb61af1071fd812706ae7f0c
Steven Bone <steven@stevenbone.com>
2017-01-11 09:50:38 -0500

    add zsh config for tmuxinator commands:   tmuxinator commands                          # Lists commands available in...   tmuxinator completions [arg1 arg2]           # Used for shell completion   tmuxinator copy [EXISTING] [NEW]             # Copy an existing project to...   tmuxinator debug [PROJECT] [ARGS]            # Output the shell commands t...   tmuxinator delete [PROJECT1] [PROJECT2] ...  # Deletes given project   tmuxinator doctor                            # Look for problems in your c...   tmuxinator help [COMMAND]                    # Describe available commands...   tmuxinator implode                           # Deletes all tmuxinator proj...   tmuxinator list                              # Lists all tmuxinator projects   tmuxinator local                             # Start a tmux session using ...   tmuxinator new [PROJECT]                     # Create a new project file a...   tmuxinator start [PROJECT] [ARGS]            # Start a tmux session using ...   tmuxinator stop [PROJECT]                    # Stop a tmux session using a...   tmuxinator version                           # Display installed tmuxinato... alias for tmuxinator

---
+++ tmux/tmuxinator.zsh
@@ -0,0 +1,29 @@
+_tmuxinator() {
+  local commands projects
+  commands=(${(f)"$(tmuxinator commands zsh)"})
+  projects=(${(f)"$(tmuxinator completions start)"})
+
+  if (( CURRENT == 2 )); then
+    _describe -t commands "tmuxinator subcommands" commands
+    _describe -t projects "tmuxinator projects" projects
+  elif (( CURRENT == 3)); then
+    case $words[2] in
+      copy|debug|delete|open|start)
+        _arguments '*:projects:($projects)'
+      ;;
+    esac
+  fi
+
+  return
+}
+
+compdef _tmuxinator tmuxinator mux
+alias mux="tmuxinator"
+
+# Local Variables:
+# mode: Shell-Script
+# sh-indentation: 2
+# indent-tabs-mode: nil
+# sh-basic-offset: 2
+# End:
+# vim: ft=zsh sw=2 ts=2 et
