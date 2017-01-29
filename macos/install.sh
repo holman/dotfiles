b96c5a2c03dba5349976072334c6702236eafed4
chauncey <cchauncey@gmail.com>
2017-01-29 13:40:51 -0600

    Isolate OS X specific commands

--- macos/install.sh
+++ macos/install.sh
@@ -3,5 +3,7 @@
 # command line interface to it that we can use to just install everything, so
 # yeah, let's do that.
 
-echo "› sudo softwareupdate -i -a"
-sudo softwareupdate -i -a
+if [ $OS = Darwin ]; then
+  echo "› sudo softwareupdate -i -a"
+  sudo softwareupdate -i -a
+fi
