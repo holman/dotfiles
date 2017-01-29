94a4090b95651d4bd7a660987293409a9c781349
chauncey <cchauncey@gmail.com>
2017-01-29 13:38:28 -0600

    Pulling over aliases, etc from old config

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,6 +6,17 @@ then
   alias git=$hub_path
 fi
 
+alias unadd='git reset HEAD'
+alias ac='!git add . && git commit -am'
+alias la='!git config -l | grep alias | cut -c 7-'
+alias l=log --pretty-format:"%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --date=short
+alias b="!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'"
+alias clean-merged='!git branch --merged | grep -v \"\\*\" | xargs -n 1 git branch -d'
+alias rao='git remote add origin'
+alias fu='!git fetch && git fetch upstream'
+alias fa='git fetch --all'
+alias fairy-pick='!git fetch && git cherry-pick'
+
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -54,11 +54,7 @@ directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-battery_status() {
-  $ZSH/bin/battery-status
-}
-
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\nin $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
