c3a2caa3ff0816fd948784f52caddc14cca55602
chaadow <chaadow@msn.com>
2017-02-07 12:13:35 +0100

    Source z utility for navigating to recent destination (Inspired by junegunn

---
+++ system/sourcing.zsh
@@ -0,0 +1,8 @@
+# Z integration with FZF
+. /usr/local/etc/profile.d/z.sh
+
+unalias z 2> /dev/null
+z() {
+  [ $# -gt 0 ] && _z "$*" && return
+  cd "$(_z -l 2>&1 | fzf --height 40% --reverse +s --tac --query "$*" | sed 's/^[0-9,.]* *//')"
+}
