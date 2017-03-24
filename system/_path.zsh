e576afc7fc70b5b00c6b98a97648b133b70f8723
Eric Ritchey <eric@eandjdesign.com>
2017-03-24 14:22:28 -0700

    adding ant_home

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,3 @@
-export PATH="~/.themekit:~/.rbenv/bin:./bin:/usr/local:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export ANT_HOME="/Users/eritchey/bin/apache-ant-1.10.1"
+export PATH="~/.themekit:~/.rbenv/bin:~/bin:./bin:/usr/local:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:$ANT_HOME"
 eval "$(rbenv init -)"
