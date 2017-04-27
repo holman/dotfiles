3aecb843e36ef0fd4f8eb104ad7c8c6433def765
crimsonknave <crimsonknave@gmail.com>
2017-04-27 14:09:02 -0400

    Adding rdp staging alias

---
+++ cybereason/aliases.zsh
@@ -0,0 +1,2 @@
+alias csc_stage='rdesktop -u Administrator -p $STAGING_PASS -g 1800x1100 54.172.104.247'
+alias csc_stage_password='echo "Enter the Stage CSC server password"; read -rs STAGING_PASS ; export STAGING_PASS'
