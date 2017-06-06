e5d1899ac0e985b402c0fcb362e6558395465e55
Benjamin Hillmann <bhillmann@outlook.com>
2017-06-06 15:03:21 -0500

    Added the init.

--- prezto/install.sh
+++ prezto/install.sh
@@ -1,6 +1,2 @@
-update_modules () {
-  # Pull the latest changes and update all submodules
-  git submodule update --init --recursive
-}
-
-setup_gitconfig
+# Pull the latest changes and update all submodules
+git submodule update --init --recursive
