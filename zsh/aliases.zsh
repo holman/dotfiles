fd6301dd19dc5791009c4dbf04778ebd1841f62e
Dan Hodges <dan.hodges@bernardhealth.com>
2017-06-15 13:00:40 -0500

    initial setup

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 alias reload!='. ~/.zshrc'
+alias f='git grep --heading --break --line-number'
+alias last-hotfix='git ls-remote --heads | grep -iE "\/hotfix-\d{4}$" | cut -d"/" -f3 | tail -n1'
+
 
 alias cls='clear' # Good 'ol Clear Screen command
