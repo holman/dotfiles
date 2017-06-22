e705eb1138e20a440964742f6fa5b2871100189c
DanHodges <dhdgs690@gmail.com>
2017-06-21 20:28:52 -0500

    hippie completion and 8080

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,9 @@ alias f='git grep --heading --break --line-number'
 alias last-hotfix='git ls-remote --heads | grep -iE "\/hotfix-\d{4}$" | cut -d"/" -f3 | tail -n1'
 
 alias myw="cd ~MyWsb"
+alias hard="git reset --hard"
+alias 8080="lsof -n -i4TCP:8080"
+
 
 
 
