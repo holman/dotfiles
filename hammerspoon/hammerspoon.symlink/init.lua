cb0d27a703b3d8b80de3b112c3440be0a81a357b
Patrick Auld <Patrick@PatrickAuld.com>
2017-06-28 17:58:14 -0700

    Fixes issues found when installing on a new computer
    Hammerspoon scripts where not handled within dotfiles. Brewfile
    was missing some required things and contained other unused ones.
    Iterm2 config was not properly linked.

---
+++ hammerspoon/hammerspoon.symlink/init.lua
@@ -0,0 +1,3 @@
+local anycomplete = require "anycomplete/anycomplete"
+anycomplete.registerDefaultBindings()local anycomplete = require "anycomplete/anycomplete"
+anycomplete.registerDefaultBindings()
