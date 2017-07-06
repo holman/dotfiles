d601c16323850d657356d98ebdefea8f8dd8c70b
Troy Rosenberg <tmr08c@gmail.com>
2017-07-06 11:54:22 -0400

    Changes Mac OS key repeat rate.
    The rate set by default was too fast and seemed to cause issues with emacs. This
    _should_ set it the fastest setting you can set it to via the interface.

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -19,8 +19,8 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 # Show the ~/Library folder.
 chflags nohidden ~/Library
 
-# Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 0
+# Set a ~really~ reasonably fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 2
 
 # Set the Finder prefs for showing a few different volumes on the Desktop.
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
