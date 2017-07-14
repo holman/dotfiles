d80884cf465c7712cb81cb5360aa984a65119b03
Cedric Michaux <cedric@he8us.be>
2017-07-14 14:35:29 +0200

    Docker aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,3 +1,7 @@
+#Eval the default docker machine env
+alias dme="eval $(docker-machine env default)"
+
+# Get docker
 alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
 
 # Kill all running containers.
@@ -9,5 +13,8 @@ alias drm='printf "\n>>> Deleting stopped containers\n\n" && docker rm $(docker 
 # Delete all untagged images.
 alias drmi='printf "\n>>> Deleting untagged images\n\n" && docker rmi $(docker images -q -f dangling=true)'
 
+# Delete all dangling volumes
+alias drmv='printf "\n>>> Deleting dangling volumes\n\n" && docker volume rm $(docker volume ls -qf dangling=true)'
+
 # Delete all stopped containers and untagged images.
-alias dclean='drm || true && drmi'
+alias dclean='drm || true && drmi || true && drmv'
