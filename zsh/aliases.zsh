6955dd6bc2915ccdfaa1a3f2974f80e22807715e
DanHodges <dhdgs690@gmail.com>
2017-08-21 18:13:59 -0500

    removed ruby and other things I don't need

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,12 +2,7 @@ alias reload!='. ~/.zshrc'
 alias f='git grep --heading --break --line-number'
 alias last-hotfix='git ls-remote --heads | grep -iE "\/hotfix-\d{4}$" | cut -d"/" -f3 | tail -n1'
 
-alias myw="cd ~MyWsb"
 alias hard="git reset --hard"
 alias 8080="lsof -n -i4TCP:8080"
-
-
-
-
 
 alias cls='clear' # Good 'ol Clear Screen command
