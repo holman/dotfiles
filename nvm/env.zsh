e8a9c5d28401d12151e82a13875045f4ab4b7af9
Cedric Michaux <cedric@he8us.be>
2017-08-31 00:16:32 +0200

    NVM setup

--- nvm/env.zsh
+++ nvm/env.zsh
@@ -1,2 +1,24 @@
 export NVM_DIR="$HOME/.nvm"
-source $NVM_DIR/nvm.sh
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+# Autoload .nvmrc
+autoload -U add-zsh-hook
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
--- nvm/install.sh
+++ nvm/install.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+) && . "$NVM_DIR/nvm.sh"
 
-if [ ! -d "$NVM_DIR" ]; then
-  git clone https://github.com/creationix/nvm.git $NVM_DIR
-fi
-
-cd $NVM_DIR
-git checkout `git describe --abbrev=0 --tags`
-
-source $NVM_DIR/nvm.sh
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 
 nvm install stable
 nvm alias default stable
 nvm use stable
 
 npm install npm@latest -g
-npm install bower grunt-cli gulp-cli webpack webpack-dev-server -g
+npm install bower grunt-cli gulp-cli webpack webpack-dev-server yarn -g
