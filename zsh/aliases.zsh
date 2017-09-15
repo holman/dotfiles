33529c215903dd3b5ad356f38be916e9ecc329a3
DanHodges <dhdgs690@gmail.com>
2017-09-15 09:38:59 -0500

    added prune-branches and bdebuild allias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,10 @@
 alias reload!='. ~/.zshrc'
 alias f='git grep --heading --break --line-number'
 alias last-hotfix='git ls-remote --heads | grep -iE "\/hotfix-\d{4}$" | cut -d"/" -f3 | tail -n1'
+alias prune-branches='git branch | grep -v "master" | xargs git branch -D'
 
 alias hard="git reset --hard"
 alias 8080="lsof -n -i4TCP:8080"
+alias bdebuild='curl -sH "Accept-encoding: gzip" https://app.dev.wordsearchbible.com/ | gunzip - | ack "(Build: v\d+)" --output="\$1"'
 
 alias cls='clear' # Good 'ol Clear Screen command
