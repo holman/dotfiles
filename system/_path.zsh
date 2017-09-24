03f6773d8c5bf27f4e7f0d7a14ef4107fbd7c947
Matthew Ceroni <matthew.ceroni@smarsh.com>
2017-09-24 12:20:42 -0700

    Updated PATH.

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="/sbin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
