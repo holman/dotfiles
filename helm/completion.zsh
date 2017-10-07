ab7ab731ad3dded7c7c64fc29bd283b9d6395674
Matthew Ceroni <matthewceroni@gmail.com>
2017-10-07 15:37:04 -0700

    Added conditional for kubectl and helm commands.

--- helm/completion.zsh
+++ helm/completion.zsh
@@ -1,1 +1,5 @@
+if (( ! $+commands[helm] ))
+then
+ return 0;
+fi
 source <(helm completion zsh)
--- kubectl/completion.zsh
+++ kubectl/completion.zsh
@@ -1,1 +1,5 @@
+if (( ! $+commands[kubectl] ))
+then
+ return 0;
+fi
 source <(kubectl completion zsh)
