57bafe1e522b1416e1f46d036bc3fb84529a997b
jwcobb <github@jcobb.org>
2017-11-13 15:44:58 -0700

    Initial commit after forking

---
+++ oh-my-zsh/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Oh My Zsh
+#
+# This installs Oh My Zsh
+
+# Check for Oh My Zsh
+#if [ ! -d "~/.oh-my-zsh" ]
+#then
+#  echo "  Installing Oh My Zsh for you."
+#
+#  # Install Oh My Zsh
+#  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#
+#fi
+#
+#exit 0
--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,27 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+# Flush the OS X DNS cache
+# @link https://support.apple.com/en-us/HT202516
+alias flush='sudo killall -HUP mDNSResponder'
+
+# Add an alias for Homestead
+# @see https://laravel.com/docs/5.2/homestead#daily-usage
+alias homestead='function __homestead() { (cd ~/Homestead && vagrant $*); unset -f __homestead; }; __homestead'
+
+# Launch homestead and ssh into the box
+alias work='homestead up && homestead ssh'
+
+# An alias for the Behat testing framework
+alias behat='vendor/bin/behat'
+
+# An alias for the phpspec testing framework
+alias phpspec='vendor/bin/phpspec'
+
+# Composer aliases
+alias cda='composer dump-autoload'
+
+# Artisan aliases
+alias a='php artisan'
+alias al='php artisan list'
