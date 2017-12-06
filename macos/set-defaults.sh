1a3b025868bcdf2d6ebb754e17e63a8461aa7eb4
tmr08c <tmr08c@gmail.com>
2017-12-06 06:48:56 -0500

    Updates OS X refernces to macOS.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,9 +1,9 @@
-# Sets reasonable OS X defaults.
+# Sets reasonable macOS defaults.
 #
-# Or, in other words, set shit how I like in OS X.
+# Or, in other words, set shit how I like in macOS.
 #
 # The original idea (and a couple settings) were grabbed from:
-#   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+#   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
