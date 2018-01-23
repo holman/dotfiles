b3161e6add19e43c77de949fec360997dec8f448
jwcobb <github@jcobb.org>
2018-01-23 15:49:57 -0700

    Remove battery status from prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -61,7 +61,8 @@ battery_status() {
   fi
 }
 
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+#export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'$(directory_name) $(git_dirty)$(need_push)%# '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
