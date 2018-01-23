1a173b02f89cdb3bc523239d9d199305a0a2ee28
jwcobb <github@jcobb.org>
2018-01-23 15:49:35 -0700

    Add some macOS aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,15 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+
+## macOS specific aliases
+if test "$(uname)" = "Darwin"; then
+    ## Flush the DNS cache Mac OS X 10.7+
+    alias flushdns='sudo killall -HUP mDNSResponder'
+
+    alias copy='pbcopy'
+    alias paste='pbpaste'
+    alias desktop="cd $HOME/Desktop"
+    alias download="cd $HOME/Downloads"
+fi
