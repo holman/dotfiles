3969dd6653db1473bd2b37c16028cc36d1e0fc0a
Eric Ritchey <eric@eandjdesign.com>
2018-02-17 17:06:16 -0800

    there's things in here that should be pushed to master

--- system/_path.zsh
+++ system/_path.zsh
@@ -3,5 +3,5 @@ export JAVA_OPTS='-Xmx2048m -Dsun.lang.ClassLoader.allowArraySyntax=true'
 export MAVEN_HOME="/usr/local/bin/mvn"
 export MAVEN_OPTS='-Xmx2048m -XX:ReservedCodeCacheSize=128m -Dsun.lang.ClassLoader.allowArraySyntax=true'
 
-export PATH="~/.rbenv/bin:~/bin:./bin:/usr/local:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:$MAVEN_HOME:$JAVA_HOME"
+export PATH="~/.rbenv/bin:~/bin:~/bin/platform-tools:./bin:/usr/local:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:$MAVEN_HOME:$JAVA_HOME"
 eval "$(rbenv init -)"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,8 @@ alias updatedb="sudo /usr/libexec/locate.updatedb"
 alias sl="ls"                               # in case you spell it wrong it will still work
 alias ll="ls -alh"                          # list all files, even the hidden ones
 alias lf="du -sh"                           # display directory file size
-alias relaod=reload                         # in case you spell it wrong it will still work
+alias relaod=reload!                        # in case you spell it wrong it will still work
+alias reload=reload!                        # in case you spell it wrong it will still work
 
 # ssh into ritcheyfamily.com
 alias rfam="ssh ritcheyer@crossingscommunity.com"
@@ -20,14 +21,18 @@ alias apachestop="sudo apachectl stop"
 
 # Opening Applications
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-alias st='open /Applications/SourceTree.app'
+alias ccall="cd ~/bin/tesla/tesla.com/drupal;./drush cc all;cd .."
 
+alias st='open /Applications/SourceTree.app'
+alias code="open /Applications/Visual\ Studio\ Code.app"
+alias subl="open /Applications/Sublime\ Text.app"
 
 
 # Switching PHP versions
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 alias switch53="brew-php-switcher 53;php -v"
 alias switch56="brew-php-switcher 56;php -v"
+alias switch70="brew-php-switcher 70;php -v"
 
 
 # editor of choice -- Follow tutorial here: https://www.sublimetext.com/docs/2/osx_command_line.html
@@ -39,3 +44,19 @@ alias switch56="brew-php-switcher 56;php -v"
 
 alias startaemauthor="ttab -a iterm -t 'AEM Author' && cd ~/bin/tesla/aem-startup/author; java -Xmx1024M -Xmx1256M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=30303,server=y,suspend=n -jar aem6-author-p4502.jar -r local,author -gui; sleep 5s; exit;"
 alias startaempublish="ttab -a iterm -t 'AEM Publisher' && cd ~/bin/tesla/aem-startup/publish; java -Xmx1024M -Xmx1256M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=30303,server=y,suspend=n -jar aem6-publish-p4503.jar -r local,author -gui; sleep 5s; exit;"
+
+alias core="cd ~/bin/tesla/particles/particles-core"
+alias playground="cd ~/bin/tesla/particles/particles-component-playground"
+
+
+# a function to copy code using Highlight:
+# https://gist.github.com/jimbojsb/1630790#gistcomment-1207389
+
+function light() {
+  if [ -z "$2" ]
+    then src="pbpaste"
+  else
+    src="cat $2"
+  fi
+  $src | highlight -O rtf --syntax $1 --font FiraCode --style darkness --font-size 24 | pbcopy
+}
