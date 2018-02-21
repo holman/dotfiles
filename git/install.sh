4c03eb612d6a2253beb64d222932642716f6cfae
Benjamin Hillmann <bhillmann@outlook.com>
2018-02-20 20:12:07 -0600

    Updated to include proper zsh loading.

--- git/install.sh
+++ git/install.sh
@@ -1,6 +1,6 @@
 # Git completion
 # Last modified 2/20/2018
 # Release version 2.16.2
-wget -O _git https://raw.githubusercontent.com/git/git/ffa952497288d29d94b16675c6789ef83850def3/contrib/completion/git-completion.zsh
-wget -O git-completion.bash https://raw.githubusercontent.com/git/git/ffa952497288d29d94b16675c6789ef83850def3/contrib/completion/git-completion.bash
-wget -O git-prompt.sh https://raw.githubusercontent.com/git/git/ffa952497288d29d94b16675c6789ef83850def3/contrib/completion/git-prompt.sh
+#wget -O _git https://raw.githubusercontent.com/git/git/ffa952497288d29d94b16675c6789ef83850def3/contrib/completion/git-completion.zsh
+#wget -O git-completion.bash https://raw.githubusercontent.com/git/git/ffa952497288d29d94b16675c6789ef83850def3/contrib/completion/git-completion.bash
+wget -O git-prompt.zsh https://raw.githubusercontent.com/git/git/ffa952497288d29d94b16675c6789ef83850def3/contrib/completion/git-prompt.sh
