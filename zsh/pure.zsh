53aae97ade6c9e059fbc7196811f2e00081170a7
Benjamin Hillmann <bhillmann@outlook.com>
2018-02-21 12:04:18 -0600

    feat: Testing the prompt setup.

--- zsh/pure.zsh
+++ zsh/pure.zsh
@@ -1,15 +1,15 @@
-fpath=( "$HOME/.zprompt" $fpath )
-autoload -U promptinit; promptinit
+# fpath=( "$HOME/.zprompt" $fpath )
+# autoload -U promptinit; promptinit
 
-# optionally define some options
-PURE_CMD_MAX_EXEC_TIME=10
+# # optionally define some options
+# PURE_CMD_MAX_EXEC_TIME=10
 
-# Safer symbols
-PURE_PROMPT_SYMBOL='»'
-PURE_GIT_DOWN_ARROW='↑'
-PURE_GIT_UP_ARROW='↓'
+# # Safer symbols
+# PURE_PROMPT_SYMBOL='»'
+# PURE_GIT_DOWN_ARROW='↑'
+# PURE_GIT_UP_ARROW='↓'
 
-#Put time in before prompt
-RPROMPT='%F{white}%*'
+# #Put time in before prompt
+# RPROMPT='%F{white}%*'
 
-prompt pure
+# prompt pure
