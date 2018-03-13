16784bd8619c1c5814e75832abcb0944ed51361f
Gytis Raciukaitis <gyr@trustpilot.com>
2018-03-13 17:50:30 +0100

    added zprezto-contrib for clipboard and additional prompts

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,8 +5,9 @@ if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
   zprezto-update
 else
-    echo "Installing zprezto"
-    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" &2>/dev/null
+  echo "Installing zprezto"
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" 2>/dev/null
+  git clone --recursive https://github.com/belak/prezto-contrib "${ZDOTDIR:-$HOME}/.zprezto/contrib" 2>/dev/null
 fi
 
 
