01e9ef380252b6cee7478f807aa0c370e5b4b3e4
Patrick Auld <Patrick@PatrickAuld.com>
2018-05-18 13:35:33 -0700

    Adds EmojiComplete Hammerspoon spoon

--- hammerspoon/hammerspoon.symlink/init.lua
+++ hammerspoon/hammerspoon.symlink/init.lua
@@ -1,3 +1,6 @@
 local anycomplete = require "anycomplete/anycomplete"
-anycomplete.registerDefaultBindings()local anycomplete = require "anycomplete/anycomplete"
 anycomplete.registerDefaultBindings()
+
+local emojicomplete = require "emojicomplete/emojicomplete"
+emojicomplete.registerDefaultBindings()
+
