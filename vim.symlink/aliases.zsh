13fdc9e83ca1a091e2cddb7893b2dfb7abf4b3a0
chaadow <chaadow@msn.com>
2018-06-10 14:01:36 +0200

    Update vim version alias

--- vim.symlink/aliases.zsh
+++ vim.symlink/aliases.zsh
@@ -1,4 +1,4 @@
 # Use the terminal version of macvim without the need of installing full XCode
 #
 # alias vim='mvim -v'
-alias vim='/usr/local/Cellar/macvim/8.0-120/MacVim.app/Contents/MacOS/Vim'
+alias vim='/usr/local/Cellar/macvim/8.0-145_1/MacVim.app/Contents/MacOS/Vim'
