bd1f750b7b114e90eb5f4ea37a0b6752315761e3
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2018-07-24 08:36:55 +0200

    Add disassemble alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,12 @@ if [[ "$(uname -s)" == "Linux" ]]; then
   alias open="xdg-open"
 fi
 
+function disas_impl() {
+  objdump -drwCS -Mintel $1 || gobjdump -drwCS -Mintel $1;
+}
+
+
 alias p4s="git p4 submit -M"
 alias gvim="gvim --remote-tab-silent"
 alias cdtmp='cd $(mktemp -d)'
+alias disas='disas_impl'
