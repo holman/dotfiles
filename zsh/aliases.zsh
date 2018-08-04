b5add65093baa6cd277a443e3fdb7d30a51196ba
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2018-08-04 16:25:05 +0200

    Add function to print a horizontal divider

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,8 +8,14 @@ function disas_impl() {
   objdump -drwCS -Mintel $1 || gobjdump -drwCS -Mintel $1;
 }
 
+func horizontal_divider() {
+  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+}
+
 
 alias p4s="git p4 submit -M"
 alias gvim="gvim --remote-tab-silent"
 alias cdtmp='cd $(mktemp -d)'
 alias disas='disas_impl'
+# Source http://wiki.bash-hackers.org/snipplets/print_horizontal_line
+alias div='horizontal_divider'
