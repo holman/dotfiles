b3a77f4a0a225b7632c100575c0eba5bfd35e37c
pattonjp <>
2018-08-20 13:49:11 -0500

    added kube config switcher

---
+++ kube/config.zsh
@@ -0,0 +1,9 @@
+switch-kubes(){
+  local kube_path="$HOME/.kube/$1"
+  if [ -f $kube_path ]; then
+    echo "using $HOME/.kube/$1"
+    ln -sf $kube_path ~/.kube/config
+  else
+    echo "cannot find config::: $kube_path"
+  fi
+}
