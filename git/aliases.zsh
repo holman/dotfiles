6df3445c9ab78fa29fa4e83ba7f73635049336d6
crimsonknave <crimsonknave@gmail.com>
2018-08-21 09:59:17 -0400

    Sign merge commits

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,6 +8,7 @@
 
 # The rest of my fun git aliases
 alias gc='git commit -v -S'
+alias gm='git merge -S' 
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
