dc805314faffe22af29d439a0f40844fc6197f9c
pattonjp <pattonjp@gmail.com>
2018-08-29 13:23:53 -0500

    k8 completions

--- kube/completion.zsh
+++ kube/completion.zsh
@@ -6,7 +6,19 @@ source_if_exists() {
     source $1
   fi
 }
-source_if_exists $HOME/.kube/completion.zsh.inc
-source_if_exists $HOME/.kops/completion.zsh.inc
-source_if_exists $HOME/.helm/completion.zsh.inc
-source_if_exists $HOME/.minikube-completion
+if [ -x "$(command -v kubectl)" ]; then
+  source <(kubectl completion zsh)
+fi
+
+if [ -x "$(command -v kops)" ]; then
+  source <(kops completion zsh)
+fi
+
+if [ -x "$(command -v helm)" ]; then
+  source <(helm completion zsh)
+fi
+
+# source_if_exists $HOME/.kube/completion.zsh.inc
+# source_if_exists $HOME/.kops/completion.zsh.inc
+# source_if_exists $HOME/.helm/completion.zsh.inc
+# source_if_exists $HOME/.minikube-completion
