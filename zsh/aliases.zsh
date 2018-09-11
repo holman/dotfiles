ebaffac637382d1c7e145a932c1a5145328520e9
DanHodges <dhdgs690@gmail.com>
2018-09-11 18:11:43 -0500

    use ag over awk because shiny

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,15 +1,14 @@
 alias reload!='. ~/.zshrc'
 alias gg='git grep --heading --break --line-number'
-alias f='ack --pager="less -FRSX"'
+alias f='ag --pager="less -FRSX"'
+alias twf='yarn run test:watch:firefox'
 
 alias last-hotfix='git ls-remote --heads | grep -iE "\/hotfix-\d{4}$" | cut -d"/" -f3 | tail -n1'
 alias prune-branches='git branch | grep -v "master" | xargs git branch -D'
 alias yr='rm -rf node_modules && yarn'
 alias ys='yarn start'
 alias ya='rm -rf node_modules && yarn && yarn start'
-alias e='emacsclient'
+alias e="emacsclient"
 alias hard="git reset --hard"
 alias 8080="lsof -n -i4TCP:8080"
 alias bdebuild='curl -sH "Accept-encoding: gzip" https://app.dev.wordsearchbible.com/ | gunzip - | ack "(Build: v\d+)" --output="\$1"'
-
-alias cls='clear' # Good 'ol Clear Screen command
