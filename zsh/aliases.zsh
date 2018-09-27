97911328c26c11eaa48cd0b93984b5ce57d7e260
DanHodges <dhdgs690@gmail.com>
2018-09-27 10:18:44 -0500

    small ideavim tweaks. Magit alias.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,6 +9,8 @@ alias yr='rm -rf node_modules && yarn'
 alias ys='yarn start'
 alias ya='rm -rf node_modules && yarn && yarn start'
 alias e="emacsclient"
+alias enw="emacsclient -nw"
 alias hard="git reset --hard"
 alias 8080="lsof -n -i4TCP:8080"
 alias bdebuild='curl -sH "Accept-encoding: gzip" https://app.dev.wordsearchbible.com/ | gunzip - | ack "(Build: v\d+)" --output="\$1"'
+alias m='emacsclient -nw --eval "(magit-status-same-window)"'
