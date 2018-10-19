172bfbe642a9b29a5a5c225cb7f897dd021bae17
Andrew Holman <andy@conspyre.com>
2018-10-19 11:27:12 -0400

    Change for windows machine

--- system/_path.zsh
+++ system/_path.zsh
@@ -4,6 +4,7 @@ export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 if [[ "$(uname)" = "Linux" ]]; then
   #alias tmux='tmux -2 -f ~/.tmux-osx.conf'
   export PATH="$HOME/.linuxbrew/bin:$PATH"
+  export PATH="$HOME/.rbenv/bin:$PATH"
 else
   #alias tmux='tmux -2'
 fi
