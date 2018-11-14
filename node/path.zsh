570bc2f40678314680584d6230f273fd870dff2b
Eddie <hershmire@users.noreply.github.com>
2018-11-13 22:41:52 -0700

    Cleanup Brewfile and move NVM setup information earlier in setup (#9)

---
+++ node/path.zsh
@@ -0,0 +1,6 @@
+# For NVM which is needed to set Node path
+# https://github.com/creationix/nvm
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
