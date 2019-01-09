a2ce2316d823170825ff6d59bc9700df08a297c8
pattonjp <pattonjp@gmail.com>
2019-01-09 11:42:48 -0600

    adding github pull rebase alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,6 +8,7 @@ fi
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
+alias gr='git pull --prune --rebase'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 
