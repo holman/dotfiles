c0a833d8fed6e206d5fc54a68de26579e9076569
Andy Holman <andy@conspyre.com>
2019-01-14 19:57:13 -0500

    Add dash key

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,6 +7,6 @@ alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gst='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias gpfl='git push --force-with-lease'
