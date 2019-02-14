7b8b643640e636d499fc4eaaf4de57d5bd8c2261
Zach Holman <zach@zachholman.com>
2019-02-14 14:56:33 -0800

    battery status check

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -55,6 +55,11 @@ directory_name() {
 }
 
 battery_status() {
+  if test ! "$(uname)" = "Darwin"
+  then
+    exit 0
+  fi
+
   if [[ $(sysctl -n hw.model) == *"Book"* ]]
   then
     $ZSH/bin/battery-status
