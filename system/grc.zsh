bbb1cdb80dba4328fd50c73a3a23371cb30d3253
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2019-04-24 12:28:30 +0200

    Prevent costly brew prefix call

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,9 @@
 # GRC colorizes nifty unix tools all over the place
 if (( $+commands[grc] )) && (( $+commands[brew] ))
 then
-  source `brew --prefix`/etc/grc.bashrc
+  brew_prefix="/usr/local"
+  if [ ! -d "${brew_prefix}" ]; then
+    brew_prefix="$(brew --prefix)"
+  fi
+  source "${brew_prefix}/etc/grc.bashrc"
 fi
