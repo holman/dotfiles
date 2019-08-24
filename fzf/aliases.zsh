9ce81db2badd763e1f7c6001eb2b3d11932a8cd7
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2019-08-24 18:11:05 +0200

    Add full fzf install + fbr function for zsh

---
+++ fzf/aliases.zsh
@@ -0,0 +1,9 @@
+# Source: https://github.com/junegunn/fzf/wiki/examples#git
+# fbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
+fbr() {
+  local branches branch
+  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
---
+++ fzf/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
