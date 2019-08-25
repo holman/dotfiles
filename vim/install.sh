2697e4a475d6691e236c917c8d3b93cf941e0e6a
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2019-08-25 20:44:42 +0200

    Remove zsh dependency from VundleInstall in vim/install

--- vim/install.sh
+++ vim/install.sh
@@ -16,5 +16,5 @@ fi
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
-zsh -c 'source $ZSH/plugins/vundle/vundle.plugin.zsh; vundle'
+vim +BundleInstall +qall
 
