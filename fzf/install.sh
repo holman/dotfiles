cf5de1d724b034f98374cac989744eae022ed115
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2019-08-25 20:24:38 +0200

    Add installation of missing dependencies for fzf

--- fzf/install.sh
+++ fzf/install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+  brew install curl wget
+elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
+  sudo apt-get install -y wget curl git
+fi
+
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
