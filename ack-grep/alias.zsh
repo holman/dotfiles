b8858435b25fd92a24f7aee34c0f080a9a56f197
kaihowl <kai@hoewelmeyer.eu>
2019-08-27 08:36:58 +0200

    Completely remove ack-grep

--- ack-grep/alias.zsh
+++
@@ -1,1 +0,0 @@
-alias ack=ack-grep
--- ack-grep/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$(uname)" == "Darwin" ]; then
-  brew install ack
-elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
-  sudo apt-get install -y ack-grep
-fi
