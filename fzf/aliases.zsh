02deedf2ab8c69846699d9d3e20b45dbf962a2f8
kaihowl <kai@hoewelmeyer.eu>
2019-09-04 15:55:00 +0200

    Allow customization of patterns for fbr alias

--- fzf/aliases.zsh
+++ fzf/aliases.zsh
@@ -2,7 +2,8 @@
 # fbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
 fbr() {
   local branches branch
-  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  pattern="**/${1:-refs/heads}/**"
+  branches=$(git for-each-ref --sort=-committerdate ${pattern} --format="%(refname:short)") &&
   branch=$(echo "$branches" |
            fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
