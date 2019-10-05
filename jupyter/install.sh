54094a458e11f37ab3832d88247247d935e3b9db
takezo39 <mark.hash@gmail.com>
2019-10-05 15:36:14 +0200

    juptyter vim binding installation

--- jupyter/install.sh
+++ jupyter/install.sh
@@ -1,2 +1,8 @@
 pip3 install jupyter_contrib_nbextensions
 jupyter contrib nbextension install --user
+# You may need the following to create the directoy
+mkdir -p $(jupyter --data-dir)/nbextensions
+# Now clone the repository
+cd $(jupyter --data-dir)/nbextensions
+git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+chmod -R go-w vim_binding
