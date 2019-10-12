72c07063caa011ff803b3117a9e5016fefed93b2
Cédric Michaux <he8us@users.noreply.github.com>
2019-10-12 04:14:43 +0200

    some updates in system (#3)

--- system/env.zsh
+++ system/env.zsh
@@ -4,15 +4,12 @@ export WD_HOME="$HOME/.wd"
 export NVM_DIR="$HOME/.nvm"
 export TMUX_DIR="$HOME/.tmux"
 export ELEANOR_DODO="$HOME/Nextcloud/Naissance/Dodo"
-export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
+export ANDROID_SDK_ROOT="/Users/he8us/Library/Android/sdk"
 export ANDROID_HOME=$ANDROID_SDK_ROOT
 export PYTHON_USER_DIR="$(python3 -m site --user-base)/bin"
 
 source "$NVM_DIR/nvm.sh"
 
-export PATH="$PATH:$HOME/.rvm/bin"
-
-
-source $(brew --prefix)/etc/grc.bashrc
+#source $(brew --prefix)/etc/grc.bashrc
 
 fpath=($WD_HOME $fpath)
--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,4 @@
 export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools"
 export PATH="/usr/local/opt/python/libexec/bin:$PATH:$PYTHON_USER_DIR"
+export PATH="$PATH:$HOME/.rvm/bin"
+export PATH="$HOME/.symfony/bin:$PATH"
