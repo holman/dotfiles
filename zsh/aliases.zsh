928793738396f5b95f01763860f83cf8190e3c8b
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2019-10-13 17:42:14 +0200

    Performance: Add benchmark analysis instructions

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,23 @@
 alias reload!='. ~/.zshrc'
 
+# Inspired by https://xebia.com/blog/profiling-zsh-shell-scripts/
+function benchmark_reload! {
+  PS4=$'\\\011%D{%s%6.}\011%x\011%I\011%N\011%e\011'
+  local out_file
+  out_file=/tmp/zshstart.$$.log
+  exec 3>&2 2>$out_file
+  setopt xtrace prompt_subst
+  for i in {1..10}; do
+    echo Run $i...
+    reload!
+  done
+  unsetopt xtrace
+  exec 2>&3 3>&-
+  echo Trace written to $out_file
+  echo Analyze with for example https://github.com/raboof/zshprof/
+}
+
+
 if which xdg-open &> /dev/null; then
   alias open="xdg-open"
 fi
