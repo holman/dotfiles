c0c234cc7b0f927b19c8c0b2281b07d0eb5326f6
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2019-10-13 15:51:53 +0200

    Performance: Add topic folders to zsh fpath in one go

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,2 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder ($DOTS/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+fpath=($DOTS/* $fpath)
