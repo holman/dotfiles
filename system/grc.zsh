cc3dc83aed5d0b9c6d96172a2424a7ae51612919
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2019-10-13 15:50:10 +0200

    Performance: Remove unused grc

--- system/grc.zsh
+++
@@ -1,9 +0,0 @@
-# GRC colorizes nifty unix tools all over the place
-if (( $+commands[grc] )) && (( $+commands[brew] ))
-then
-  brew_prefix="/usr/local"
-  if [ ! -d "${brew_prefix}" ]; then
-    brew_prefix="$(brew --prefix)"
-  fi
-  source "${brew_prefix}/etc/grc.bashrc"
-fi
