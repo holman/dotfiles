cd5fce185896e32235923e57c020276c5e8f33d2
lmcgartland <luke.mcgartland@me.com>
2019-10-20 00:20:27 -0700

    Atom config

---
+++ atom/install.sh
@@ -0,0 +1,43 @@
+#!/bin/zsh
+
+# editor
+apm i highlight-selected
+apm i autoclose-html
+apm i atom-beautify
+apm i color-picker
+apm i pigments
+apm i emmet
+apm i sort-lines
+apm i trailing-spaces
+
+#ide
+apm i atom-ide-ui
+apm i ide-css
+apm i ide-json
+apm i ide-python
+
+# tabs
+apm i new-tab
+apm i tab-title
+
+# linting
+apm i linter
+apm i linter-csslint
+apm i linter-eslint
+
+# syntax highlighting
+apm i stylus
+apm i language-dots
+apm i language-babel
+apm i atom-jade
+
+# big features
+apm i merge-conflicts
+
+# general
+apm i text-manipulation
+apm i open-recent
+apm i wordcount
+apm i minimap
+apm i minimap-pigments
+apm i minimap-highlight-selected
