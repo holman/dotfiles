feb78204a416ac0ca2bda220a02878f0a6884ffd
chaadow <chaadow@msn.com>
2019-11-28 16:35:24 +0100

    Set sane mission control value

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -387,7 +387,7 @@ defaults write com.apple.dock show-process-indicators -bool true
 defaults write com.apple.dock launchanim -bool false
 
 # Speed up Mission Control animations
-defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dock expose-animation-duration -float 1
 
 # Don’t group windows by application in Mission Control
 # (i.e. use the old Exposé behavior instead)
