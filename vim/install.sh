b0d846ed1f837459ae84db2ba354b86d05a49723
takezo39 <mark.hash@gmail.com>
2019-12-11 12:18:43 +0100

    install syntastic

--- vim/install.sh
+++ vim/install.sh
@@ -1,3 +1,6 @@
 echo "installing vim pathogen"
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+cd ~/.dotfiles
