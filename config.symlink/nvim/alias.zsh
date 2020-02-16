af76459d08e2beec7b2891935ca800544cfb6706
tmr08c <tmr08c@gmail.com>
2020-02-16 12:04:18 -0500

    Makes neovim more of the default.
    * Sets up editor env variables to use neovim when available
    * Sets vim to be neovim when available
    
    Just setting vim to neovim didn't seem to make git play nice (event
    after trying to unset the `core.editor` config opion) and use neovim, so
    I also set `GIT_EDITOR`.

---
+++ config.symlink/nvim/alias.zsh
@@ -0,0 +1,5 @@
+if $(type nvim &>/dev/null)
+then
+  alias vim="nvim"
+fi
+alias v="vim ."
--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,8 @@
-export EDITOR='code'
+if $(type nvim &>/dev/null)
+then
+  export EDITOR='nvim'
+  export GIT_EDITOR='nvim'
+else
+  export EDITOR='vim'
+  export GIT_EDITOR='vim'
+fi
