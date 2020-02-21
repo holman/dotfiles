47b7f25250e13bbc9b0106c21e2579ddb71a6e70
takezo39 <mark.hash@gmail.com>
2020-02-21 11:27:04 +0100

    install supertab

--- vim/install.sh
+++ vim/install.sh
@@ -8,6 +8,11 @@ if cd ~/.vim/bundle/syntastic;
   then git pull; 
   else git clone --depth=1 https://github.com/vim-syntastic/syntastic.git; 
 fi
+cd ~/.vim/bundle
+if cd ~/.vim/bundle/supertab; 
+  then git pull; 
+  else git clone --depth=1 git@github.com:ervandew/supertab.git;
+fi
 # get checkers for syntastic
 pip install pyflakes --user
 cd ~/.dotfiles
