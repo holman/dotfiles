448067bb4e542689ab95b88bb3680746d6d63a20
pattonjp <pattonjp@gmail.com>
2020-03-03 14:42:10 -0600

    fix installs

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,7 @@
 
-# curl -sLf https://spacevim.org/install.sh | bash
+curl -sLf https://spacevim.org/install.sh | bash
 
 sv_dir=$HOME/.SpaceVim.d
 mkdir -p $sv_dir
 
-ln -s "$(pwd)/vim/spacevim.toml" $sv_dir/init.toml
+ln -sf "$(pwd)/vim/spacevim.toml" $sv_dir/init.toml
