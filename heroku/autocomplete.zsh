983be331a18871a2fcefbac17d2926819017aa4c
Ole Christian Langfjæran <judoole@gmail.com>
2020-03-03 20:57:06 +0100

    Add heroku autocomplete

---
+++ heroku/autocomplete.zsh
@@ -0,0 +1,3 @@
+
+# heroku autocomplete setup
+HEROKU_AC_ZSH_SETUP_PATH=/Users/judoole/Library/Caches/heroku/autocomplete/zsh_setup && test -f $HEROKU_AC_ZSH_SETUP_PATH && source $HEROKU_AC_ZSH_SETUP_PATH;
