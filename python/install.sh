5ddce4c8c79a49960b476866bff063f7467a320f
r-richmond <rrichmond.gh@gmail.com>
2020-03-18 09:53:14 -0700

    Use pipenv for global python packages

--- python/install.sh
+++ python/install.sh
@@ -3,5 +3,12 @@
 # avoid --user https://docs.brew.sh/Homebrew-and-Python
 pip3 install -q --upgrade pip;
 pip3 install -q --upgrade setuptools;
-pip3 install -q flake8;
-pip3 install -q black;
+pip3 install -q pipenv;
+
+CURRENT_DIRECTORY=$(pwd)
+PYTHON_DIRECTORY="$DOTFILES_ROOT/python"
+cd "$PYTHON_DIRECTORY" || { echo "change to python directory failed"; exit; }
+pipenv lock --pre;
+pipenv install --system;
+cd "$CURRENT_DIRECTORY" || { echo "change to original directory failed"; exit; }
+echo "finished installing global python packages"
