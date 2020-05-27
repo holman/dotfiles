99bb3b5a83a0501087eab9449b33786a3d42c973
takezo39 <mark.hash@gmail.com>
2020-05-27 21:20:25 +0200

    fix dump

--- homebrew/homebrewdump.sh
+++ homebrew/homebrewdump.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 cd ~/.dotfiles/homebrew
 rm Brewfile
+# need to add to PATH: https://github.com/Homebrew/homebrew-bundle/issues/237
+PATH=/usr/local/bin:${PATH}
+#/usr/local/bin/brew bundle dump 2>&1 | tee log  (debugging)
 /usr/local/bin/brew bundle dump
