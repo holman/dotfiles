d6889f78bade0dc4a61068adca4f0bd0f523d7ab
Lance Cooper <lwcooper@gmail.com>
2020-07-02 09:33:07 +1200

    Developers institute shortcuts

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,3 +1,3 @@
-alias d='docker $*'
-alias dc='docker-compose $*'
-alias dcr='docker-compose down && docker-compose build && docker-compose up'
+# alias d='docker $*'
+# alias dc='docker-compose $*'
+# alias dcr='docker-compose down && docker-compose build && docker-compose up'
