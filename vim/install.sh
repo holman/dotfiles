6f0e0dd02856fbaacb4ae87c9be129ec6d4f1180
takezo39 <mark.hash@gmail.com>
2020-07-09 12:44:41 +0200

    install black plugin

--- vim/install.sh
+++ vim/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "installing vim pathogen"
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 cd ~/.vim/autoload
@@ -16,6 +18,15 @@ if cd ~/.vim/bundle/supertab;
   then git pull; 
   else git clone --depth=1 git@github.com:ervandew/supertab.git;
 fi
+
+echo "installing vim black plugin"
+mkdir -p ~/.vim/bundle/black/plugin
+curl https://raw.githubusercontent.com/psf/black/master/plugin/black.vim -o ~/.vim/bundle/black/plugin/black.vim
+vim /dev/null -c 'q'  # make black venv
+# solve issue https://github.com/psf/black/issues/1293
+source ~/.vim/black/bin/activate  # make sure to install in the right venv
+pip install --upgrade git+https://github.com/psf/black.git
+deactivate
 
 # get checkers for syntastic
 pip install pyflakes --user
