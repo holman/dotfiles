e929d465ba2966f4499855b4a15840055930d51c
crimsonknave <crimsonknave@github.com>
2020-07-20 12:31:19 -0400

    Updating ssh aliases for titles and re-use

--- github/aliases.zsh
+++ github/aliases.zsh
@@ -1,1 +1,24 @@
-alias ssh-setup='eval `ssh-agent -s`;ssh-add'
+function ssh-setup() {
+  if [[ ! -z $SSH_AGENT_PID ]] && ps -p $SSH_AGENT_PID > /dev/null
+  then
+    echo 'Agent running'
+  else
+    eval `ssh-agent -s`
+    ssh-add
+  fi
+  set-title 'SSH Enabled'
+}
+
+function shellme() {
+  ssh-setup
+  set-title 'Shell Host'
+  ssh shell
+  set-title 'SSH Enabled'
+}
+
+function vaultme() {
+  ssh-setup
+  set-title 'Vault Host'
+  ssh vault
+  set-title 'SSH Enabled'
+}
