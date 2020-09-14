8ef25db0f736e08db259c64508b62f339b34b37e
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2020-09-14 21:58:20 +0200

    Switch to vim-plug, introduce lazy loading
    This brought the start up time down from about 180-200 ms to about 150
    ms on the MBA. Furthermore, this plugin manager is maintained compared
    to Vundle. Finally, the parallel installing/updating with multiple
    threads is a real time saver.

--- nvim/install.sh
+++ nvim/install.sh
@@ -10,11 +10,11 @@ elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   sudo snap install --beta nvim --classic
 fi
 
-# update packages in vundle
-# install vundle if not already installed
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-  "$(dirname "$0")/../git/install.sh"
-  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# update packages in Plug
+# install plug if not already installed
+plug_dir="${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim
+if [ ! -d "$plug_dir" ]; then
+  curl -fLo "${plug_dir}" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
-nvim +BundleInstall +qall
+nvim +PlugUpdate +qall
 
