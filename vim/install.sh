5a4e80ffcefcdd4087df774d58a2ec0c8706e590
takezo39 <mark.hash@gmail.com>
2020-09-21 21:03:04 +0200

    remove black installation

--- vim/install.sh
+++ vim/install.sh
@@ -22,14 +22,14 @@ if cd ~/.vim/bundle/supertab;
   else git clone --depth=1 git@github.com:ervandew/supertab.git;
 fi
 
-echo "installing vim black plugin"
-mkdir -p ~/.vim/bundle/black/plugin
-curl https://raw.githubusercontent.com/psf/black/master/plugin/black.vim -o ~/.vim/bundle/black/plugin/black.vim
-vim /dev/null -c 'q'  # make black venv
+#echo "installing vim black plugin"
+#mkdir -p ~/.vim/bundle/black/plugin
+#curl https://raw.githubusercontent.com/psf/black/master/plugin/black.vim -o ~/.vim/bundle/black/plugin/black.vim
+#vim /dev/null -c 'q'  # make black venv
 # solve issue https://github.com/psf/black/issues/1293
-source ~/.vim/black/bin/activate  # make sure to install in the right venv
-pip install --upgrade git+https://github.com/psf/black.git
-deactivate
+#source ~/.vim/black/bin/activate  # make sure to install in the right venv
+#pip install --upgrade git+https://github.com/psf/black.git
+#deactivate
 
 # get checkers for syntastic
 pip install pyflakes --user
