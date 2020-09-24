81c76c64cb67290e0f08a61365194de2c5990744
takezo39 <mark.hash@gmail.com>
2020-09-24 17:08:30 +0200

    -y for apt install

--- pyenv/install.sh
+++ pyenv/install.sh
@@ -7,7 +7,7 @@ set -e
 #sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target / ???
 
 # pyenv deps
-sudo apt-get install git python-pip make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl
+sudo apt-get install git python-pip make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl -y
 sudo pip install virtualenvwrapper
 
 if cd ~/.pyenv;
@@ -17,6 +17,6 @@ fi
 cd ~/.dotfiles
 
 # python build deps
-sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 
 
