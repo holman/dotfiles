cffde676b061ce9d6c3baab06a0d3ab6e2b25df5
Lucas McGartland <luke.mcgartland@me.com>
2020-11-05 20:40:46 -0800

    Remove Atom packages and update homebrew oneswq

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -10,15 +10,7 @@ if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
 
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  fi
-
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 exit 0
