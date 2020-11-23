03a9e5157a45790a40b24a22c8e7421e34de40ed
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2020-11-23 18:21:30 +0100

    Try without local path to python3

--- virtualenvwrapper/workon.zsh
+++ virtualenvwrapper/workon.zsh
@@ -1,4 +1,4 @@
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Devel
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+export VIRTUALENVWRAPPER_PYTHON=python3
 source /usr/local/bin/virtualenvwrapper_lazy.sh
