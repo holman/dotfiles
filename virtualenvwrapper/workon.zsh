98bd32b3f580b6739bd7fbf021b2de061ddf9ce8
Kai Hoewelmeyer <Kai.Hoewelmeyer@tomtom.com>
2020-11-25 07:57:27 +0100

    Remove platform-dependent python3 setting

--- virtualenvwrapper/workon.zsh
+++ virtualenvwrapper/workon.zsh
@@ -1,4 +1,6 @@
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Devel
-export VIRTUALENVWRAPPER_PYTHON=python3
+# Missing expansion of VIRTUALENVWRAPPER_PYTHON
+# This needs to be specific to each platform, i.e., where python3 sits.
+# Set this instead in your .zprofile
 source /usr/local/bin/virtualenvwrapper_lazy.sh
