59b76804b99f60ee84d2d73d0e9b1dc32d39fe15
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2021-01-02 12:31:24 +0100

    feat: use proper terminfo on MacOS
    Inspiration: https://medium.com/@dubistkomisch/how-to-actually-get-italics-and-true-colour-to-work-in-iterm-tmux-vim-9ebe55ebc2be

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ "$(uname)" == "Darwin" ]; then
   brew install tmux
+  terminfofile="$(dirname "$0")/tmux-256color.terminfo"
+  tic -x "$terminfofile"
 elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   sudo apt-get install -y tmux
 fi
