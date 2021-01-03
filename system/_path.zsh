be249914f960eaaccadb9057d2436f18f5245176
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2021-01-03 18:13:12 +0100

    fix!: user defined binaries take precedence over /usr/local

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTS/bin:$PATH"
+export PATH="$DOTS/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
