e6c230d05937cf84cc21a75105d63792868e5199
harrig12 <cait.harrigan@mail.utoronto.ca>
2021-01-13 18:46:13 -0500

    alias update

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,6 +1,5 @@
 # The rest of my fun git aliases
 alias gpl='git pull'
-alias gl = 'git log'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gpsh='git push origin HEAD'
 
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,3 @@
 alias reload!='. ~/.zshrc'
-
 alias cls='clear' # Good 'ol Clear Screen command
+alias la='ls -a'
