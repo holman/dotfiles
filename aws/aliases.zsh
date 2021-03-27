2e5446d9ff45c60387a8a392da3d0220750a576a
Bradley Wagner <bradley.wagner@hannonhill.com>
2021-03-26 22:18:50 -0400

    Removing properties from aws in favor of .env-vars

--- aws/aliases.zsh
+++ aws/aliases.zsh
@@ -1,6 +1,3 @@
-# Populate an aws.properties in this directory with the variables used in this script
-source "$ZSH/aws/aws.properties"
-
 alias devbox-start='aws ec2 start-instances --instance-ids $AWS_I_ID && sleep 10 && aws ec2 associate-address --instance $AWS_I_ID --public-ip $AWS_IP'
 alias devbox-status='aws ec2 describe-instance-status --instance-ids $AWS_I_ID'
 alias devbox-stop='aws ec2 stop-instances --instance-ids $AWS_I_ID'
