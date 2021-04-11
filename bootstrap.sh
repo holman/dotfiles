7096e7d75b330d7ebf6804c9accb89d8b2d75d8a
Ghasem Naddaf <ghasemnaddaf@users.noreply.github.com>
2021-04-10 23:45:28 -0700

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -d /Library ]; then
+  echo "Bootstraping Mac ..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew install git
+else
+fi
