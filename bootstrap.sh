01b10d9e4ac57e7690c925c9ddf9b5eeff4a360c
Ghasem Naddaf <ghasemnaddaf@users.noreply.github.com>
2021-04-13 08:27:49 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -33,7 +33,7 @@ curl -X POST -H "Accept: application/vnd.github.v3+json" \
   -d "{\"key\": \"${KEY}\"}"
 
 git_clone ghasemnaddaf/dotfiles $HOME
+pushd $HOME/dotfiles && ./script/bootstrap && popd
+
 /bin/bash -c "REMOTE=git@github.com:ghasemnaddaf/ohmyzsh.git BRANCH=my_changes \
     $(curl -fsSL https://raw.githubusercontent.com/ghasemnaddaf/ohmyzsh/my_changes/tools/install.sh)"
-
-./script/bootstrap
