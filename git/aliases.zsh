b94b3a4bf0e30b1857bb72391f5cad06678aeab2
harrig12 <cait.harrigan@mail.utoronto.ca>
2021-04-14 03:30:10 -0400

    keep +- in diff

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,10 +4,11 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 alias gpsh='git push origin HEAD'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
+alias gd='git diff --color'
 
 alias gr='git rm -r'
 alias ga='git add'
 alias gc='git commit -m'
 alias gac='git commit -am'
+alias gcb='git checkout'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
