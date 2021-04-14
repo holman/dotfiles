2ccfd8a06d6a1c9a2685bf2b35b0278e96b292a7
Ghasem Naddaf <ghasemnaddaf@users.noreply.github.com>
2021-04-14 12:58:01 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ function git_clone() {
   git clone git@github.com:$1.git $2
 }
 
-PKG_LIST_COMMON="git zsh"
+PKG_LIST_COMMON="git teleport zsh"
 PKG_LIST_MAC="jq"
 PKG_LIST_CASK_MAC="iterm2 virtualbox google-chrome"
 PKG_LIST_LINUX="build-essential"
@@ -16,11 +16,18 @@ if test "$(uname)" = "Darwin"; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   brew install ${PKG_LIST_COMMON} ${PKG_LIST_MAC}
   brew install --cask ${PKG_LIST_CASK_MAC}
-else
+else 
   echo "Bootstraping Linux ..."
+  
+  # repo for teleport
+  curl https://deb.releases.teleport.dev/teleport-pubkey.asc | sudo apt-key add -
+  sudo add-apt-repository 'deb https://deb.releases.teleport.dev/ stable main'
+
+  sudo apt-get update
   DEBIAN_FRONTEND=noninteractive sudo apt-get install ${PKG_LIST_COMMON} ${PKG_LIST_LINUX} 
 fi
 
+# key and git
 cat /dev/zero | ssh-keygen -q -f $HOME/.ssh/id_rsa -N ""
 
 mkdir -p $HOME/.git
@@ -31,12 +38,15 @@ curl -X POST -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token $GPAT" https://api.github.com/user/keys \
   -d "{\"key\": \"${KEY}\"}"
 
+# dotfiles
 git_clone ghasemnaddaf/dotfiles $HOME/.dotfiles
 pushd $HOME/.dotfiles && ./script/bootstrap && popd
 
+# ohmyzsh
 /bin/bash -c "ZSH= REMOTE=git@github.com:ghasemnaddaf/ohmyzsh.git BRANCH=my_changes \
     $(curl -fsSL https://raw.githubusercontent.com/ghasemnaddaf/ohmyzsh/my_changes/tools/install.sh)"
 
+# python
 PIP_INSTALL="yq"
 sudo pip3 install ${PIP_INSTALL}
 
