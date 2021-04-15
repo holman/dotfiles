3202700dadcb14e1de413cf1827fd9ee01a6f3d7
Ghasem Naddaf <ghasemnaddaf@users.noreply.github.com>
2021-04-15 15:30:33 -0700

    Update install.sh

--- k8s/install.sh
+++ k8s/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-curl -sfL https://get.k3s.io | sh -
-mkdir -p $HOME/.kube
-ln -s /etc/rancher/k3s/k3s.yaml $HOME/.kube/config
+if test "$(uname)" = "Darwin"; then
+    brew install minikube
+else
+    curl -sfL https://get.k3s.io | sh -
+    mkdir -p $HOME/.kube
 
+    ln -s /etc/rancher/k3s/k3s.yaml $HOME/.kube/k3s.config
+fi
 exit 0
