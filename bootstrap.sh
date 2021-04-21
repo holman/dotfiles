55068923cb8f6486cae01e74caf1d1196fd34f94
Ghasem Naddaf <ghasemnaddaf@users.noreply.github.com>
2021-04-21 15:59:24 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ function git_clone() {
   git clone git@github.com:$1.git $2
 }
 
-PKG_LIST_COMMON="awscli git packer teleport vim zsh"
+PKG_LIST_COMMON="awscli git packer teleport vim wget zsh"
 PKG_LIST_MAC="jq"
 PKG_LIST_CASK_MAC="docker google-chrome iterm2 virtualbox"
 PKG_LIST_LINUX="build-essential cmake curl dmidecode docker.io python3 python3-pip virtualbox"
