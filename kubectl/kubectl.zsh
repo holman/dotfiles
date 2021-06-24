968b911021b922f4f1dca34cd059b27e392da8d1
Patrick Auld <Patrick@PatrickAuld.com>
2021-06-24 15:02:04 -0700

    Removes kubectl

--- kubectl/kubectl.zsh
+++
@@ -1,7 +0,0 @@
-# Autocompletion for kubectl, the command line interface for Kubernetes
-#
-# Author: https://github.com/pstadler
-
-if [ $commands[kubectl] ]; then
-  source <(kubectl completion zsh)
-fi
