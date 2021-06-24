261d42b2f9d5e313125c34d1569e23273e6b9436
Patrick Auld <Patrick@PatrickAuld.com>
2021-06-24 14:24:55 -0700

    Adds Source Code Pro Nerd font to install and iTerm

--- fonts/install.sh
+++ fonts/install.sh
@@ -2,4 +2,7 @@
 echo "Download Source Code Pro Font:"
 echo "https://github.com/adobe-fonts/source-code-pro/releases/tag/2.030R-ro%2F1.050R-it"
 
+echo "Download Source Code Pro Nerd Font:"
+echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SourceCodePro.zip"
+
 echo "TODO: Automate this"
