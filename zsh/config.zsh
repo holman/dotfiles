3b23aa7d33c31beb0b8e52b9c08aa96317017c6a
avidan-H <mincrodo@gmail.com>
2021-08-26 17:11:01 -0400

    Cleanup and update zsh dir

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -30,10 +30,3 @@ setopt HIST_REDUCE_BLANKS
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 setopt complete_aliases
-
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^?' backward-delete-char
