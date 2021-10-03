b6e5d84b544cff3ef2bda86b0d205592ffab2810
Kai Howelmeyer <kai@hoewelmeyer.eu>
2021-10-03 20:03:18 +0200

    fix: add missing build-essential for ccache test (#34)

--- ccache/install.sh
+++ ccache/install.sh
@@ -4,6 +4,8 @@ if [ "$(uname)" == "Darwin" ]; then
   source $DOTS/common/brew.sh
   brew_install ccache
 elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
-  sudo apt-get install -y ccache
+  # Installing build-essential to have a working compiler for the test.sh
+  # Not necessary on macOS as a full llvm is installed
+  sudo apt-get install -y ccache build-essential
 fi
 
