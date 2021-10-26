5b77df27dceddad2fa90552862abe8be99e4a5cd
Kai Howelmeyer <kai@hoewelmeyer.eu>
2021-10-26 08:10:49 +0200

    chore: update ripgrep to 13.0.0 (#58)

--- ripgrep/install.sh
+++ ripgrep/install.sh
@@ -7,6 +7,12 @@ elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   apt_install curl
   tmpfile=$(mktemp)
   trap "rm -rf ${tmpfile}" EXIT
-  curl -Lo "${tmpfile}" https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
+  curl -Lo "${tmpfile}" https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+  expect_hash="95e37f20c20cae79338cc040697a7dc4113891e8"
+  actual_hash="$(sha1sum ${tmpfile} | cut -d' ' -f 1)"
+  if [[ "$expect_hash" != "$actual_hash" ]]; then
+    echo "sha1sum mismatch for ripgrep. Aborting."
+    exit 1
+  fi
   sudo dpkg -i "${tmpfile}"
 fi
