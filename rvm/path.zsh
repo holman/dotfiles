75596104f9e145151bfc3ae5c660b266a9cbf200
Michael Kuehl <hello@txs.vc>
2021-11-30 21:07:44 +0100

    added RVM config

---
+++ rvm/path.zsh
@@ -0,0 +1,5 @@
+if (( $+commands[rvm] ))
+then
+  export PATH="$PATH:$HOME/.rvm/bin"
+  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+fi
