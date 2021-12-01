dd7a3f3330826d5092468b6398ad0aa8e10420f6
Michael Kuehl <hello@txs.vc>
2021-12-01 08:12:05 +0100

    changes to zshrc

--- rvm/path.zsh
+++ rvm/path.zsh
@@ -1,5 +1,7 @@
 if (( $+commands[rvm] ))
 then
+  export rvm_max_time_flag=20 # increase timeout for install
   export PATH="$PATH:$HOME/.rvm/bin"
+
   [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 fi
