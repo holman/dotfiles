626904277e3aab562554d6b151207557b9c19b2e
prescottprue <scott@sideinc.com>
2022-02-27 11:39:50 -0700

    fix: remove atom editor

---
+++ Brewfile
@@ -0,0 +1,27 @@
+cask_args appdir: '/Applications'
+
+tap 'homebrew/bundle'
+
+brew 'ack'
+brew 'caddy'
+brew 'coreutils'
+cask 'iterm2'
+cask 'docker'
+cask 'google-cloud-sdk'
+cask 'keka' # File archiver for formats such as .rar, .7z
+brew 'direnv'
+brew 'gh'
+brew 'go'
+brew 'jq'
+brew 'wget'
+brew 'yarn'
+# brew 'youtube-dl' # Only really for personal
+
+# cask 'caffeine' # Only really for personal
+# cask 'dbvisualizer' # Use more up to date database viz
+
+# cask 'licecap' # Giphy Capture
+# cask 'postman' # there is an app now
+# cask 'protonvpn' # not the VPN I use
+# cask 'spectacle' # no longer maintained
+# cask 'visual-studio-code' # already installed
--- node/install.sh
+++ node/install.sh
@@ -1,3 +1,29 @@
+#!/bin/sh
+
+# Install NVM and then Node
+if [ ! -d "$NVM_DIR" ]; then
+  # Install manually: https://github.com/creationix/nvm#manual-install
+  echo "› Installing nvm (Node Version Manager)"
+
+  export NVM_DIR="$HOME/.nvm" && (
+    git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+    cd "$NVM_DIR"
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+  ) && \. "$NVM_DIR/nvm.sh"
+
+  echo " Installing latest Node LTS as default"
+  nvm install --lts
+else
+  echo "› Upgrading NVM (Node Version Manager)"
+   # Upgrade manually: https://github.com/creationix/nvm#manual-upgrade
+  (
+    cd "$NVM_DIR"
+    git fetch --tags origin
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+  ) && \. "$NVM_DIR/nvm.sh"
+fi
+
+# https://www.npmjs.com/package/spoof
 if test ! $(which spoof)
 then
   if test $(which npm)
