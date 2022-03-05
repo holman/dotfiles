14355c3bb6a3a011c8dad0a27c503d0ee9b5a06a
Eddie Leffler <757524+hershmire@users.noreply.github.com>
2022-03-05 14:51:09 -0700

    fix: update homebrew install and re-add go paths

---
+++ go/paths.zsh
@@ -0,0 +1,2 @@
+export GOPATH=$PROJECTS/go
+export PATH="$GOPATH/bin:$PATH"
--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,16 +9,7 @@
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
-
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-  fi
-
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 exit 0
