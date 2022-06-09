ca47c7f708bb28963324cdf6928d4f748ad4b48a
Rebecca Miller-Webster <rmw@github.com>
2022-06-09 18:00:18 -0500

    commenting out stuff i don't need

--- php/install.sh
+++ php/install.sh
@@ -1,17 +1,17 @@
-if test $(command -v brew); then
-  if test $(command -v composer); then
-    echo 'Upgrading php & composer...'
-    brew upgrade php composer
-  else
-    echo 'Installing php & composer ...'
-    brew install php composer
-  fi
+# if test $(command -v brew); then
+#   if test $(command -v composer); then
+#     echo 'Upgrading php & composer...'
+#     brew upgrade php composer
+#   else
+#     echo 'Installing php & composer ...'
+#     brew install php composer
+#   fi
 
-  if test $(command -v wp-cli); then
-    echo 'Upgrade wp-cli'
-    brew upgrade wp-cli
-  else
-    echo 'Installing wp-cli'
-    brew install wp-cli
-  fi
-fi
+#   if test $(command -v wp-cli); then
+#     echo 'Upgrade wp-cli'
+#     brew upgrade wp-cli
+#   else
+#     echo 'Installing wp-cli'
+#     brew install wp-cli
+#   fi
+# fi
--- python/install.sh
+++ python/install.sh
@@ -1,18 +1,18 @@
-if test $(command -v brew); then
-  if test $(python3 -V | grep -q 'Python 3'); then
-    echo "   installing python"
-    brew install python
-  else
-    echo " upgrading python"
-    brew upgrade python
-  fi
+# if test $(command -v brew); then
+#   if test $(python3 -V | grep -q 'Python 3'); then
+#     echo "   installing python"
+#     brew install python
+#   else
+#     echo " upgrading python"
+#     brew upgrade python
+#   fi
 
-  # https://installvirtual.com/how-to-install-python-3-8-on-mac-using-pyenv/
-  if test ! $(command -v pyenv); then
-    echo "  installing pyenv"
-    brew install pyenv
-  else
-    echo "  upgrading pyenv"
-    brew upgrade pyenv
-  fi
-fi
+#   # https://installvirtual.com/how-to-install-python-3-8-on-mac-using-pyenv/
+#   if test ! $(command -v pyenv); then
+#     echo "  installing pyenv"
+#     brew install pyenv
+#   else
+#     echo "  upgrading pyenv"
+#     brew upgrade pyenv
+#   fi
+# fi
