9f4bb939c91da91184cf69fe3512db67a9841260
Jupiter <jupiter@jupiter.local>
2022-07-09 10:03:24 -0700

    First commit

--- zsh/completion.zsh
+++
@@ -1,5 +0,0 @@
-# matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
-# pasting with tabs doesn't perform completion
-zstyle ':completion:*' insert-tab pending
