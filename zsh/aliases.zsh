8e3c73bbe3fd65cded3432dac32c6c9e713edded
Marc Høegh <mhoegh90@gmail.com>
2022-08-19 07:37:19 +0200

    Remove the restart part of the world-alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,4 +2,4 @@ alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
 
-alias world='zdi vm restart && zdi services restart && zdi apps restart'
+alias world='zdi services restart && zdi apps restart'
