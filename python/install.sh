b46f57a189483ce8e0f763846b640e0410c7cd8f
r-richmond <rrichmond.gh@gmail.com>
2022-11-27 14:55:09 -0800

    Python adjustments

--- python/install.sh
+++ python/install.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
 # avoid --user https://docs.brew.sh/Homebrew-and-Python
-pip3 install -q --upgrade pip;
-pip3 install -q --upgrade setuptools;
-pip3 install -q pipenv;
+pip3 install -q --upgrade pip
+pip3 install -q --upgrade setuptools
+pipx install pipenv
 
 CURRENT_DIRECTORY=$(pwd)
 PYTHON_DIRECTORY="$DOTFILES_ROOT/python"
-cd "$PYTHON_DIRECTORY" || { echo "change to python directory failed"; exit; }
-pipenv lock --pre;
-pipenv install --system;
-cd "$CURRENT_DIRECTORY" || { echo "change to original directory failed"; exit; }
+cd "$PYTHON_DIRECTORY" || {
+  echo "change to python directory failed"
+  exit
+}
+pipenv lock --pre
+pipenv install --system
+cd "$CURRENT_DIRECTORY" || {
+  echo "change to original directory failed"
+  exit
+}
 echo "finished installing global python packages"
+
+pipx install black
+pipx install flake8
+pipx install isort
+pipx install pre-commit
