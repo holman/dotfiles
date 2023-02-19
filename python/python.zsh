eaba70af0c3c16dad92866a4ca3a375bbd3404f9
r-richmond <rrichmond.gh@gmail.com>
2023-02-19 13:14:41 -0800

    tweak

--- python/python.zsh
+++ python/python.zsh
@@ -1,4 +1,4 @@
 # Make Python use UTF-8 encoding for output to stdin, stdout, and stderr.
-export PYTHONIOENCODING='UTF-8';
+export PYTHONIOENCODING='UTF-8'
 
-alias pip=/opt/homebrew/bin/pip3;
+# alias pip=/opt/homebrew/bin/pip3;
