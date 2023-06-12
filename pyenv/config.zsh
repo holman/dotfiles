31793d9c494c43ec21c4f20f4dfaf38c29a873dd
Kevin Terusaki <kterusaki@salesforce.com>
2023-06-12 10:31:27 -0700

    fix pyenv virtualenv and pyenv path

--- pyenv/config.zsh
+++ pyenv/config.zsh
@@ -1,3 +1,5 @@
+# for some reason adding pyenv in PATH is not recognized in this eval statement
+# calling pyenv & pyenv-virtualenv-init directly from homebrew install location
 eval "$(/usr/local/bin/pyenv init -)"
 eval "$(/usr/local/bin/pyenv-virtualenv-init -)"
 
--- pyenv/path.zsh
+++ pyenv/path.zsh
@@ -1,2 +1,5 @@
 export PYENV_ROOT="$HOME/.pyenv"
-export PATH="/usr/local/bin/pyenv":$PATH
+# According to pyenv README, we need to add this but `.pyenv/bin` doesn't exist and seems
+# to work perfectly fine without adding this. Others have noted similar results. Leaving here
+# until documentation updates or a fix has been pushed for pyenv
+export PATH="$PYENV_ROOT/bin":$PATH
