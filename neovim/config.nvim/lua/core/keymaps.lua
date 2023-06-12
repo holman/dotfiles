41a19545af758b1073f81851d08d0f47c02653dd
Kyle Kartchner <kkartch@gmail.com>
2023-06-12 16:26:45 -0600

    Added vim option setting to allow cut and paste across os apps using yank and put

--- neovim/config.nvim/lua/core/keymaps.lua
+++ neovim/config.nvim/lua/core/keymaps.lua
@@ -8,6 +8,9 @@ vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
 
+-- Allow cut and paste between tmux and terminal sessions using yank and put
+vim.opt.clipboard = 'unnamed'
+
 -- use spaces for tabs
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
