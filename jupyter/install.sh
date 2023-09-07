542fb200be8945b661d0b5f4003a7c0ac1937437
takezo39 <mark.hash@gmail.com>
2023-09-07 12:24:30 +0200

    remove installation of old jupyter vim binding with nbextensions

--- jupyter/install.sh
+++ jupyter/install.sh
@@ -10,18 +10,3 @@ ln -sf ~/jupyter_notebook_config.json ~/.jupyter/
 # install in .pyenv/ rather than using --user. `pyenv global XXX` needs to have been set
 echo "installing jupyter extensions"
 pip3 install jupyter
-pip3 install jupyter_contrib_nbextensions
-jupyter contrib nbextension install
-# You may need the following to create the directoy
-mkdir -p $(jupyter --data-dir)/nbextensions
-# Now clone the repository
-cd $(jupyter --data-dir)/nbextensions
-if cd vim_binding
-  then
-    git pull
-  else
-    git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
-    chmod -R go-w vim_binding
-fi
-cd $(jupyter --data-dir)/nbextensions
-
