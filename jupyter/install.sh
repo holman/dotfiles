276dd673f179a14057abec7fde6047307a2a6561
takezo39 <mark.hash@gmail.com>
2023-09-07 12:26:50 +0200

    remove old jupyter passoword config

--- jupyter/install.sh
+++ jupyter/install.sh
@@ -4,8 +4,6 @@ set -e
 
 echo "copying jupyter config"
 cp ~/.dotfiles/jupyter/jupyter_notebook_config.py ~/.jupyter/
-echo "symlink jupyter password setup"
-ln -sf ~/jupyter_notebook_config.json ~/.jupyter/
 
 # install in .pyenv/ rather than using --user. `pyenv global XXX` needs to have been set
 echo "installing jupyter extensions"
