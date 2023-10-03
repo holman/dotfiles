ad91af50271c954a5da602a596ad024b7d169d71
Ole Christian Langfjæran <judoole@gmail.com>
2023-10-03 13:28:01 +0200

    Can't remember

--- go/path.zsh
+++ go/path.zsh
@@ -1,2 +1,2 @@
-export GOPATH=$PROJECTS/go
-export PATH="$GOPATH/bin:$PATH"
+#export GOPATH=$PROJECTS/go
+#export PATH="$GOPATH/bin:$PATH"
--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,4 +1,3 @@
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-export LC_ALL='en_US.UTF-8'
-export LANG='en_US.UTF-8'
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
