1bd31bb988ad355130964d27a1f6e7c088d2960c
True Merrill <true.merrill@gtri.gatech.edu>
2024-03-23 12:21:53 -0400

    Updating dot -> dotfiles to avoid conflict with graphviz

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,12 +6,11 @@
 # using Homebrew.
 
 # Check for Homebrew
-if test ! $(which brew) then
+if test ! $(which brew); then
   echo "  Installing Homebrew for you."
 
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin" then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  if test "$(uname)" = "Darwin"; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 
 fi
