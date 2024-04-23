3974c1e329d77f144e9f531f1d5004b52a1d525d
Steve <github@cavender.io>
2024-04-23 13:17:12 -0400

    fix bun installer

--- bun/install.sh
+++ bun/install.sh
@@ -1,1 +1,14 @@
-curl -fsSL https://bun.sh/install | zsh
+#!/bin/sh
+#
+# Bun
+#
+
+# Check for bun
+if test ! $(which bun)
+then
+  echo "  Installing Bun for you."
+
+  ruby -e "$(curl -fsSL https://bun.sh/install | bash)"
+fi
+
+exit 0
