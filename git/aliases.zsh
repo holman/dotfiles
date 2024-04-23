35e8f79e3b8314dcbbd821e5dfa8d2210e373e38
Steve <github@cavender.io>
2024-04-23 11:29:32 -0400

    add omz git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,7 +9,6 @@ fi
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
@@ -19,6 +18,25 @@ alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
+
+# omz git aliases
+alias g='git'
+alias ga='git add'
+alias gcl='git clone --recurse-submodules'
+alias gcam='git commit --all --message'
+alias gd='git diff'
+alias gdca='git diff --cached'
+alias gdcw='git diff --cached --word-diff'
+alias gds='git diff --staged'
+alias gdw='git diff --word-diff'
+alias gdup='git diff @{upstream}'
+alias gf='git fetch'
+alias glog='git log --oneline --decorate --graph'
+alias gm='git merge'
+alias gl='git pull'
+alias gp='git push'
+alias grev='git revert'
+alias gst='git status -sb'
+alias gsw='git switch'
