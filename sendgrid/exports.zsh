78073ca3c842ad00118fba533b9071e679e4a2c1
ccnixon <cnixon@twilio.com>
2024-07-01 14:45:18 -0500

    Add s3mime function

--- sendgrid/exports.zsh
+++ sendgrid/exports.zsh
@@ -1,1 +1,22 @@
 export BOOTSTRAP_SCRIPT_DIR=/Users/cnixon/Code/src/github.com/sendgrid/sg-workstation/scripts
+
+
+s3mime () {
+	dir="/tmp"
+	local s3url=$1
+	local env=$(echo "$1" | cut -d '/' -f 3 | cut -d '-' -f 3)
+	local filename=$(echo "$1" | cut -d '/' -f 4)
+	if [[ "$env" == "staging" ]]; then
+			if aws-okta exec pre_prod_eng_coreplatform -- aws s3 cp "$s3url" "$dir/$filename"; then
+					code "$dir/$filename"
+			else
+					echo "Failed to download or open file."
+			fi
+	else 
+			if aws-okta exec prod_eng_coreplatform -- aws s3 cp "$s3url" "$dir/$filename"; then
+					code "$dir/$filename"
+			else
+					echo "Failed to download or open file."
+			fi
+	fi
+}
