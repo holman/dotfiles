4c53a8d13db4f1722fe56b8dd06ffd6cd47d6c9b
Steve <github@cavender.io>
2024-11-18 11:26:03 -0500

    make vim/tmux plugin scripts more robust

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
 #
-# Tmux 
+# Tmux
 #
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Variables
+REPO_URL="git@github.com:tmux-plugins/tpm.git"
+REPO_DIR="~/.tmux/plugins/tpm"
+
+# Check if the directory exists and is a Git repository
+if [ -d "$REPO_DIR" ]; then
+    if [ -d "$REPO_DIR/.git" ]; then
+        echo "Directory $REPO_DIR exists and is a Git repository."
+        echo "Fetching the latest changes..."
+        cd "$REPO_DIR" || exit
+        git fetch --all
+        git pull
+    else
+        echo "Directory $REPO_DIR exists but is not a Git repository. Exiting."
+        exit 1
+    fi
+else
+    echo "Directory $REPO_DIR does not exist. Cloning the repository..."
+    git clone "$REPO_URL" "$REPO_DIR"
+fi
+
 exit 0
