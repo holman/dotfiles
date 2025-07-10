f9ba0b851c0efc2a4c2c895be3916a5bb80f7172
Ole Christian Langfjæran <judoole@gmail.com>
2025-07-10 13:39:02 +0200

    Add some nvm

---
+++ node/nvm.zsh
@@ -0,0 +1,3 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
