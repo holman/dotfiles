b407dfd710a63988baa3009ab683628d251fecfd
Ole Christian Langfjæran <judoole@gmail.com>
2025-07-10 13:38:35 +0200

    Remove hub

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,11 +1,3 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
-
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
