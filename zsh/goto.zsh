d9790190575ddf4af24c3b371292c61e3b8d5fc6
Dang Mai <contact@dangmai.net>
2026-04-23 14:54:37 -0700

    [zsh] better goto function

--- zsh/goto.zsh
+++ zsh/goto.zsh
@@ -5,45 +5,52 @@ fn_exists() {
 }
 
 if ! fn_exists goto; then
-  PROJECT_DIR=${PROJECTS:-"~/dev"}
+  : ${PROJECT_DIR:=~/dev}
+
   goto() {
-    case $1 in
+    local input=$1
+    local parts=("${(@s:/:)input}")
+    local host user repo
 
-      */*)
-        navigate $1
-      ;;
-
+    case ${#parts[@]} in
+      3)
+        host=$parts[1]
+        user=$parts[2]
+        repo=$parts[3]
+        ;;
+      2)
+        host=github.com
+        user=$parts[1]
+        repo=$parts[2]
+        ;;
+      1)
+        host=github.com
+        user=${GO_DEFAULT:-${GH_ORG_DEFAULT}}
+        repo=$parts[1]
+        ;;
       *)
-        navigate ${GO_DEFAULT:-${GH_ORG_DEFAULT}}/$1
-      ;;
-
+        echo "usage: goto [<host>/]<user>/<repo> | <repo>"
+        return 1
+        ;;
     esac
-  }
 
-  git_clone() {
-    for repo in $@; do
-      if [[ "$repo" == */* ]]; then
-        local dir=$PROJECT_DIR/src/github.com/$repo
-        git clone ssh://git@github.com/$repo.git $dir
-        cd $dir
-      else
-        local dir=$PROJECT_DIR/src/github.com/${USER_DEFAULT:-${GH_ORG_DEFAULT}}/$repo
-        git clone ssh://git@github.com/${USER_DEFAULT:-${GH_ORG_DEFAULT}}/$repo.git $dir
-        cd $dir
-      fi
-    done
-  }
-
-  navigate() {
-    cd ${PROJECT_DIR}/src/github.com/$1 &> /dev/null ||
-    git_clone $1 2> /dev/null ||
-    echo "repo @ $1 does not exist"
+    local dir=$PROJECT_DIR/src/$host/$user/$repo
+    if [[ -d $dir ]]; then
+      cd $dir
+    elif git clone ssh://git@$host/$user/$repo.git $dir; then
+      cd $dir
+    else
+      echo "repo @ $host/$user/$repo does not exist"
+      return 1
+    fi
   }
 
   _zsh_goto() {
-    base=$PROJECT_DIR/src/github.com/
-    compadd $(find $base -maxdepth 2 -mindepth 2 | sed "s|$base/||")
-    compadd $(find $base/${GO_DEFAULT:-${GH_ORG_DEFAULT}} -maxdepth 1 -mindepth 1 | sed "s|$base/${GO_DEFAULT:-${GH_ORG_DEFAULT}}/||")
+    local base=$PROJECT_DIR/src
+    local default=${GO_DEFAULT:-${GH_ORG_DEFAULT}}
+    compadd $(find $base -maxdepth 3 -mindepth 3 -type d 2>/dev/null | sed "s|$base/||")
+    compadd $(find $base/github.com -maxdepth 2 -mindepth 2 -type d 2>/dev/null | sed "s|$base/github.com/||")
+    compadd $(find $base/github.com/$default -maxdepth 1 -mindepth 1 -type d 2>/dev/null | sed "s|$base/github.com/$default/||")
   }
 
   if type compdef &> /dev/null ; then
